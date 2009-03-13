4ff0c954d8d23fca9c10b651ed33f89f81221d76
eddie <eddie@1f418930-7ff8-0310-b8ad-c653122473bc>
2009-03-13 18:09:28 +0000

    fixing a couple small bugs with pill game
    git-svn-id: http://svn.ccnmtl.columbia.edu/smart/trunk@14106 1f418930-7ff8-0310-b8ad-c653122473bc

--- pill_game/media/js/choose_treatment_line.js
+++ pill_game/media/js/choose_treatment_line.js
@@ -9,18 +9,28 @@ function select_line(which) {
    removeElementClass(line_to_select, 'line_unselected');
    addElementClass(line_to_unselect, 'line_unselected');
    removeElementClass(line_to_unselect, 'line_selected');
-   
    game_state['treatment_line'] = which;
+   clear_pill_info();
    Intervention.saveState();
 }
 
 function init() {
     game_state = Intervention.getGameVar('pill_game_state',  default_state);
-    if (game_state['treatment_line'] != null ) {
+    if (game_state['treatment_line'] != "" ) {
         select_line(game_state['treatment_line'])
     }
     connect ('select_line_1', 'onclick', partial (select_line, 1));
     connect ('select_line_2', 'onclick', partial (select_line, 2));
+    clear_pill_info()
 }
 
 
+
+function clear_pill_info () {
+   // erase all pill info every time a client switches lines -- otherwise stuff gets confusing.
+    game_state.selected_meds  = [];
+    delete game_state.night_pills;
+    delete game_state.day_pills;
+    delete game_state.day_pills_time_menu_selected_index;
+    delete game_state.night_pills_time_menu_selected_index;
+}
