0cdd004bdd15bb1ac5b8bb37de57e1de18210c3e
sky <sky@1f418930-7ff8-0310-b8ad-c653122473bc>
2009-03-17 16:04:22 +0000

    admin restore/backup
    git-svn-id: http://svn.ccnmtl.columbia.edu/smart/trunk@14178 1f418930-7ff8-0310-b8ad-c653122473bc

--- media/js/static_auth/admin.js
+++ media/js/static_auth/admin.js
@@ -91,7 +91,7 @@ http://www.josh-davis.org/pythonAES
 
 	restorals = false;
 	function makeRestoralLink(name,hilite) {
-	    name = A({'id':name,'href':'#'+name+'','onclick':"UserAdmin.restore('"+name+"')"},name);
+	    name = A({'id':name,'href':'#'+name+'','onclick':"showElement('restoring-please-wait');UserAdmin.restore('"+name+"')"},name);
 	    var attrs = null;
 	    if (hilite) {
 		//attrs = {'class':'hilite'};
@@ -247,9 +247,7 @@ http://www.josh-davis.org/pythonAES
 
     UserAdmin.prototype.restore = function(backup_key) {
 	var self = this;
-	if(confirm('This will delete all current client/account data, \
-                    and replace it with the restoring data. \
-                    Are you sure?')) {
+	if(confirm('This will delete all current client/account data, and replace it with the restoring data. Are you sure?')) {
 	    var backup_string = self.session.permStor[backup_key].value;
 	    var plaintext = self.decrypt(backup_string);
 	    var the_package = evalJSON(plaintext);
@@ -263,6 +261,8 @@ http://www.josh-davis.org/pythonAES
 	    for (a in the_package) {
 		self.session.permStor[a] = the_package[a];
 	    }
+	    hideElement('restoring-please-wait');
+	    alert('Data successfully restored from backup!');
 	}
     }
 
