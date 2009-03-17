e5d377c0bec98f29d4f7d46910b4497273c34391
eddie <eddie@1f418930-7ff8-0310-b8ad-c653122473bc>
2009-03-17 15:03:16 +0000

    ok, ONE last fix
    git-svn-id: http://svn.ccnmtl.columbia.edu/smart/trunk@14164 1f418930-7ff8-0310-b8ad-c653122473bc

--- pill_game/media/js/choose_treatment_line.js
+++ pill_game/media/js/choose_treatment_line.js
@@ -2,15 +2,18 @@ var game_state;
 
 addLoadEvent(init);
 
-function select_line(which) {
-   line_to_select = (which == 1 ? 'select_line_1' : 'select_line_2');
-   line_to_unselect =  (which == 1 ? 'select_line_2' : 'select_line_1');
+function select_line(new_line) {
+   if (game_state['treatment_line']  != new_line) {
+        //logDebug ("Changing.");
+        clear_pill_info();
+   }
+   line_to_select = (new_line == 1 ? 'select_line_1' : 'select_line_2');
+   line_to_unselect =  (new_line == 1 ? 'select_line_2' : 'select_line_1');
    addElementClass(line_to_select, 'line_selected');
    removeElementClass(line_to_select, 'line_unselected');
    addElementClass(line_to_unselect, 'line_unselected');
    removeElementClass(line_to_unselect, 'line_selected');
-   game_state['treatment_line'] = which;
-   clear_pill_info();
+   game_state['treatment_line'] = new_line;
    Intervention.saveState();
 }
 
@@ -21,12 +24,12 @@ function init() {
     }
     connect ('select_line_1', 'onclick', partial (select_line, 1));
     connect ('select_line_2', 'onclick', partial (select_line, 2));
-    clear_pill_info()
 }
 
 
 
 function clear_pill_info () {
+    logDebug ("CLEARING");
    // erase all pill info every time a client switches lines -- otherwise stuff gets confusing.
     game_state.selected_meds  = [];
     delete game_state.night_pills;
