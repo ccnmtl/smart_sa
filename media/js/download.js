fd5ba6253d318fac7fe726629e0557522ca55fed
sky <sky@1f418930-7ff8-0310-b8ad-c653122473bc>
2009-09-25 13:29:57 +0000

    make it a button to start download
    git-svn-id: http://svn.ccnmtl.columbia.edu/smart/trunk@16909 1f418930-7ff8-0310-b8ad-c653122473bc

--- media/js/download.js
+++ media/js/download.js
@@ -1,25 +1,43 @@
-var def = doXHR('cache-manifest.txt');
 var file_count;
-def.addCallback(function(xhr) {
-    var files = xhr.responseText.split("\n");
-    var file_count = files.length;
-    window.updateCount = function() {
-	--file_count;
-	if (file_count==0) {
-	    $('done').style.backgroundColor = 'green';
-	    alert('All Done!  You can go offline now.');
-	}
+var donedone = -1;
+
+function finishedCaching(evt) {
+    $('cacheDone').style.backgroundColor = 'green';
+    registerDone();
+}
+
+function registerDone() {
+    if (++donedone) {
+	alert('All Done!  You can go offline now.');
     }
-    var iframe_string = '';
-    forEach(files,function(line) {
-	if (/\.pdf$/.test(line)) {
+}
+
+connect(applicationCache,'oncached',finishedCaching);
+
+
+function downloadAllPages() {
+    var def = doXHR('cache-manifest.txt');
+    def.addCallback(function(xhr) {
+	var files = xhr.responseText.split("\n");
+	file_count = files.length;
+	window.updateCount = function() {
 	    --file_count;
-	    return;
+	    if (file_count==0) {
+		$('pagesDone').style.backgroundColor = 'green';
+		registerDone();
+	    }
 	}
-	iframe_string += '<iframe src="'+line+'" onload="updateCount()" width="1" height="1"></iframe>';
+	var iframe_string = '';
+	forEach(files,function(line) {
+	    if (!(/\.html/.test(line))) {
+		--file_count;
+		return;
+	    }
+	    iframe_string += '<iframe src="'+line+'" onload="updateCount()" width="1" height="1"></iframe>';
+	});
+	$("iframe-dumping-ground").innerHTML = iframe_string;
     });
-    $("iframe-dumping-ground").innerHTML = iframe_string;
-});
+}
 
 //not allowed  TestRunner.hta, user0extensions.js.sample,js.jar,,custom_rhino.jar, gotapi.py, make_docs.py, pack.py, domains.json, intervention_storage.js~, build.py, Color.rst, Signal.rst, DragAndDrop.rst, index.rst, selenium*, mochikit/tests, mochikit/doc/ (includes rsts)
 /*
