af26d28d300f368ab16b8ba11d79b36d57abe9bc
sky <sky@1f418930-7ff8-0310-b8ad-c653122473bc>
2010-04-15 17:06:07 +0000

    clean CSVs for researcher viewing
    git-svn-id: http://svn.ccnmtl.columbia.edu/smart/trunk@18787 1f418930-7ff8-0310-b8ad-c653122473bc

--- scripts/dump_csvs.py
+++ scripts/dump_csvs.py
@@ -12,25 +12,25 @@ def ssnmtree_dict(dic,id=None):
     for k,v in dic.items():
         if not isinstance(v,dict):
             continue
-        tree.append([id,k,
+        tree.append(id +[k,
                      v['disclosure'],
                      v['support'],
-                     v['name'],])
+                     v['name'][0:1],])
 
     return tree
 
 def pillgame_dict(dic,id=None):
     meds = []
     for v in dic['day_pills'].values():
-        meds.append([id,v['where'],v['pill_type'], ])
+        meds.append(id +[v['where'],v['pill_type'], ])
     for v in dic['night_pills'].values():
-        meds.append([id,v['where'],v['pill_type'], ])
+        meds.append(id +[v['where'],v['pill_type'], ])
     return meds
 
 def problemsolving_dict(dic,id=None):
     my_issues = []
     for k,v in dic['my_issues'].items():
-        my_issues.append([id,k,
+        my_issues.append(id +[k,
                           v.get('action',''),
                           v.get('aim',''),
                           v.get('alternatives',''),
@@ -44,21 +44,44 @@ def timelog_dict(dic,id=None):
     logs = []
     for date,pages in dic.items():
         for page in pages:
-            logs.append([id, date, page['page'], page['time'], ])
+            logs.append(id+[date, page['page'], page['time'], ])
     return logs
+
+#users that were tests or admins
+user_black_list = [
+    "USER_4e984ae0fa98ceecb5fdb3db80fd265a55fb6d0a",
+    "USER_d035ce90cf96be0fa6c91a36ef2e1d00e461d336",
+    "USER_15c9983829fe923e298cd4cf4e4767d0b1eb4e43",
+    "USER_769e3afd72746555185c2e66705bc0897ab6e025",
+    "USER_5601d1fc537373afb9d021f5743eb99f8491de99",
+    "USER_2ac506652286b4f89fb44940e8fa5f9ce898dab6",
+    "USER_c528642096fa0242b8930dadff0c30822b0591af",
+    "USER_e886af071c9a8745c6145bf2b03ef6ff5b8ffc68",
+    "USER_4afe44e3a373d52a070140962b037bf02c08d37a",
+    "USER_672fcb6ac2f8fbd233918fbe4ce23735c212ad3e",
+    "USER_267cec60ea376dcf874551ea03b24f9fa85bf91e",
+    "USER_e2ea40b14c51fd43c1e193052314020d4c5a8557",
+    "USER_d2ca85bc2f22a9866cdaea9fca59d1e254092e81",
+    "USER_fa4d413862abcd1571e6c009624950277a98ddcd",
+    "USER_b1c2d721c00db749d28614ae68dffa3be5c457fd",
+    "USER_030c222b6d1d60284504658c6172cf043147c5b8",
+    "USER_2d682ba0bc87c8d194534c575e15e36b29432c8c",
+    "USER_d15f6ed60f293f659451bee296ea36507ce5b7bf",
+    "USER_b1c2d721c00db749d28614ae68dffa3be5c457fd",
+    ]
 
 exceptions = {
     'pill_game_state':{
         'path':'games.pill_game_state',
         'select':['day_pills_time_menu_selected_index','night_pills_time_menu_selected_index','treatment_line'],
         'new_table':pillgame_dict,
-        'table_cols':['Patient ID','Time of Day','Pill'],
+        'table_cols':['Time of Day','Pill'],
         },
     'problemsolving':{
         'path':'games.problemsolving',
         'select':['chosen-issue','default_page'],
         'new_table':problemsolving_dict,
-        'table_cols':['Patient ID','Issue',
+        'table_cols':['Issue',
                       'Action','Aim','Alternatives',
                       'Alternatives2','Alternatives3','Ask',],
         },
@@ -66,7 +89,7 @@ exceptions = {
         'path':'games.ssnmtree',
         'select':[],
         'new_table':ssnmtree_dict,
-        'table_cols':['Patient ID','Position','Disclosure','Support','Name']
+        'table_cols':['Position','Disclosure','Support','Name']
         },
     'sessions':{
         'select':[],
@@ -78,11 +101,18 @@ exceptions = {
         'path':'timelog',
         'select':[],
         'new_table':timelog_dict,
-        'table_cols':['Patient ID','Date','Page','Seconds on page']
-        }
+        'table_cols':['Date','Page','Seconds on page']
+        },
+    'firstname':{
+        'select':[],
+        },
+    'fullname':{
+        'select':[],
+        },
 }
 
 def dict_fields(dic):
+    "recursively return fields that are not"
     to_return = []
     for k,v in dic.items():
         if k in exceptions:
@@ -111,6 +141,7 @@ def getval(dic,keys):
     return val
 
 def cols(rows):
+    "returns unique list of all column names"
     m = set()    
     for r in rows:
         data = json.loads(r[1])
@@ -126,9 +157,11 @@ def csv_info(rows):
     extras = {}
     for k,v in exceptions.items():
         if v.has_key('new_table'):
-            extras[k] = [ v['table_cols'] ] #header
+            extras[k] = [['Patient ID','patientnumber']+v['table_cols'] ] #header
 
-    for r in rows:    
+    for r in rows:
+        if r[0] in user_black_list:
+            continue
         user = [r[0]]
         data = json.loads(r[1])
         for col in main_file_cols:
@@ -137,7 +170,7 @@ def csv_info(rows):
         for k,v in extras.items():
             edata = getval(data,exceptions[k]['path'])
             if edata:
-                v.extend( exceptions[k]['new_table'](edata, r[0]) )
+                v.extend( exceptions[k]['new_table'](edata, [r[0],data['patientnumber']]) )
 
     return main,extras
 
