ab7b3c59762c990ea1001aba70c10a39029c7d4e
Schuyler Duveen <sky@skyb.us>
2011-02-23 14:54:01 -0500

    upgrading to Django 1.2 and new virtualenv/bootstrap

--- bootstrap.py
+++ bootstrap.py
@@ -2,14 +2,20 @@
 import os
 import sys
 import subprocess
+import shutil
 
 pwd = os.path.dirname(__file__)
 vedir = os.path.join(pwd,"ve")
 
-try:
-    os.removedirs(vedir)
-except:
-    pass
+if os.path.exists(vedir):
+    shutil.rmtree(vedir)
 
-subprocess.call(["python",os.path.join(pwd,"create-ve.py")])
-subprocess.call(["python",os.path.join(pwd,"ve-bootstrap.py"),vedir])
+subprocess.call(["python",os.path.join(pwd,"pip.py"),"install",
+                 "-E",os.path.join(pwd,"ve"),
+                 "--enable-site-packages",
+                 "--requirement",os.path.join(pwd,"requirements/apps.txt")])
+
+# do not want to try to install the egg on 2.5
+if sys.version.startswith("2.6"):
+    subprocess.call([os.path.join(vedir,"bin/easy_install"),
+                     os.path.join(pwd,"requirements/eggs/egenix_mx_base-3.1.3-py2.6-linux-x86_64.egg")])
