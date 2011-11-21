54b846a76ab01af5f08d0b1cb63b7979588151d6
Anders Pearson <anders@columbia.edu>
2011-11-21 15:19:35 -0500

    working towards full lettuce + terrain + selenium

--- intervention/features/index.feature
+++ intervention/features/index.feature
@@ -4,4 +4,6 @@ Feature: Rocking with lettuce and django
         Given I access the url "/"
         Then I see the header "Welcome to Masivukeni!"
 
-
+    Scenario: Index Page Load 2
+        Given I access the url "/home.html"
+        Then I see the header "Welcome to Masivukeni!"
---
+++ terrain.py
@@ -0,0 +1,13 @@
+from lettuce import *
+from lettuce.django import django_url
+from selenium import selenium
+
+#@before.harvest
+#def prepare_browser_driver(variables):
+#    if variables.get('run_server', False) is True:
+#        world.browser = selenium('localhost', 8000, '*firefox', django_url('/'))
+#        world.browser.start()
+
+#@after.harvest
+#def shutdown_browser_driver(results):
+#    world.browser.stop()
