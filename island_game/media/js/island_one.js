04cbef64db8b880af50504c81899edcf17e9b451
Anders Pearson <anders@columbia.edu>
2012-02-07 18:06:51 -0500

    appeasing jshint

--- island_game/media/js/island_one.js
+++ island_game/media/js/island_one.js
@@ -1,6 +1,37 @@
 // how high  the guy is standing on the island
 var guy_on_island_offset = 150;
+var game_state;
+var bottom_of_game;
 
+var recalc = function () {
+    //adjust water level:
+    var water_level = (
+        sliders.bad1.get() * sliders.bad1.influence +
+        sliders.bad2.get() * sliders.bad2.influence) / 2;
+    sliders.water.set(water_level);
+
+    //adjust island level:
+    var island_level = (sliders.good.get() * sliders.good.influence);
+    sliders.island.set(island_level);
+    var island_height = sliders.island.pos(sliders.island.get())[1];
+
+    //pick an image for the dude:
+    var dude_images = (Intervention.current_user.gender === "M") ? man_images : woman_images;
+    var altitude = 0.5 + (sliders.island.getfraction() - sliders.water.getfraction()) / 2;
+    $('dude').src = pick_image(altitude, dude_images);
+
+    //adjust the dude
+    setStyle('dude', {
+        "left":  ($('dude').x) + 'px',
+        "top" : (island_height - guy_on_island_offset) + 'px',
+        'position': 'absolute',
+        'height': '184px',
+        'width': '110px'
+      });
+
+    clip_image($('island_moving'), 600, bottom_of_game);
+    clip_image($('water_moving'), 600, bottom_of_game);
+  };
 
 function init() {
     game_state = Intervention.getGameVar('island_game_state', default_state);
@@ -9,21 +40,20 @@ function init() {
              function (a) { new Slider(a); }
     );
 
-    logDebug (serializeJSON(game_state));
-    logDebug (game_state.page_1_bad1);
-
+    logDebug(serializeJSON(game_state));
+    logDebug(game_state.page_1_bad1);
 
     if (game_state.page_1_bad1 !== null) {
-      logDebug ("yo");
+      logDebug("yo");
       sliders.bad1.set(game_state.page_1_bad1);
       sliders.bad2.set(game_state.page_1_bad2);
       sliders.good.set(game_state.page_1_good);
     }
 
-    sliders.bad1.draggable.options.onchange = recalc ;
-    sliders.bad2.draggable.options.onchange = recalc ;
-    sliders.good.draggable.options.onchange = recalc ;
-    mystery_factor = 20; // where does this come from?
+    sliders.bad1.draggable.options.onchange = recalc;
+    sliders.bad2.draggable.options.onchange = recalc;
+    sliders.good.draggable.options.onchange = recalc;
+    var mystery_factor = 20; // where does this come from?
     bottom_of_game = elementDimensions($('sky')).h  + elementPosition($('sky')).y - mystery_factor;
     recalc();
   }
@@ -35,43 +65,15 @@ function save_state() {
     Intervention.saveState();
   }
 
-function recalc() {
-    //adjust water level:
-    water_level = (
-        sliders.bad1.get() * sliders.bad1.influence +
-        sliders.bad2.get() * sliders.bad2.influence) /2;
-    sliders.water.set(water_level);
-
-    //adjust island level:
-    island_level = (sliders.good.get() * sliders.good.influence );
-    sliders.island.set(island_level);
-    island_height = sliders.island.pos(sliders.island.get())[1];
-
-    //pick an image for the dude:
-    dude_images = (Intervention.current_user.gender == "M") ? man_images : woman_images;
-    altitude = 0.5 + (sliders.island.getfraction() - sliders.water.getfraction()) / 2;
-    $('dude').src = pick_image (altitude, dude_images);
-
-    //adjust the dude
-    setStyle( 'dude', {
-        "left":  ( $('dude').x  ) + 'px',
-        "top" : ( island_height - guy_on_island_offset) + 'px',
-        'position':'absolute',
-        'height': '184px',
-        'width': '110px'
-      } );
-
-    clip_image ($('island_moving'), 600, bottom_of_game);
-    clip_image ($('water_moving'), 600, bottom_of_game);
-  }
 
 addLoadEvent(init);
 
 
 //this function snaps the graphics back into place on browser window resize
 //from http://forums.port80.asn.au/archive/index.php/t-8475.html
-window.onresize= reloadme;
-function reloadme(){ //or whatver else you have
+
+function reloadme() { //or whatver else you have
     save_state();
-    setTimeout(function () {window.location.reload();},1); //
+    setTimeout(function () { window.location.reload(); }, 1); //
   }
+window.onresize = reloadme;
--- island_game/media/js/slider.js
+++ island_game/media/js/slider.js
@@ -1,14 +1,19 @@
-function setpos (thing, x, y) {
-  setStyle( thing,  { "left":  ( x  ) + 'px', "top" : ( y ) + 'px', 'position':'absolute' } );
+function setpos(thing, x, y) {
+  setStyle(thing,  { "left":  (x) + 'px', "top": (y) + 'px', 'position': 'absolute'});
 }
 sliders = {};
 
-function Slider (settings) {
+function Slider(settings) {
   //setup
-  its = partial (getNodeAttribute, settings);
-  ints = function (n) {if (its(n) == null) return null; return parseInt (its(n)); };
+  var its = partial(getNodeAttribute, settings);
+  var ints = function (n) {
+    if (its(n) === null) {
+      return null;
+    }
+    return parseInt(its(n));
+  };
   this.id = its('id');
-  this.background_element_id=its("background_element_id");
+  this.background_element_id = its("background_element_id");
   this.image_offset = {x: ints('image_offset_x'), y: ints('image_offset_y') };
   this.start_offset = {x: ints('start_offset_x'), y: ints('start_offset_y') };
   this.end_offset =   {x: ints('end_offset_x'),   y: ints('end_offset_y') };
@@ -18,14 +23,14 @@ function Slider (settings) {
   this.z_index = ints('z_index');
   this.starting_value = ints('starting_value');
   this.influence = ints('influence');
-  this.moving = IMG({'src': this.moving_image_path, 'id': this.id + '_moving', });
-  if (this.z_index != null) {
-    logDebug ("Setting " + this.id + " to " + this.z_index );
+  this.moving = IMG({'src': this.moving_image_path, 'id': this.id + '_moving' });
+  if (this.z_index !== null) {
+    logDebug("Setting " + this.id + " to " + this.z_index);
     setStyle(this.moving, {'z-index': this.z_index});
   }
 
   //register this slider
-  sliders[this.id] = this
+  sliders[this.id] = this;
   this.background_position = getElementPosition(this.background_element_id);
 
   //draw fixed and moving components:
@@ -35,11 +40,11 @@ function Slider (settings) {
 
 
   this.fixed_image_path = its('fixed_image_path');
-  if (this.fixed_image_path != null) {
-    logDebug (this.id);
+  if (this.fixed_image_path !== null) {
+    logDebug(this.id);
     this.fixed = IMG({'src': this.fixed_image_path, 'id': this.id + '_fixed', 'z-index': this.z_index});
     appendChildNodes(doc.body, this.fixed);
-    setpos (this.fixed,
+    setpos(this.fixed,
       this.image_offset.x + this.background_position.x,
       this.image_offset.y + this.background_position.y
     );
@@ -52,113 +57,113 @@ function Slider (settings) {
   range = this.max_value - this.min_value;
   this.unit = {
     x: (this.end_offset.x - this.start_offset.x) / range,
-    y: (this.end_offset.y - this.start_offset.y) / range,
-  }
+    y: (this.end_offset.y - this.start_offset.y) / range
+  };
 
 
-  this.draggable_by_user = (its("draggable_by_user") != "false")
-  if ( this.draggable_by_user) {
-    logDebug (this.id + "is draggable by user. Setting up draggable.");
+  this.draggable_by_user = (its("draggable_by_user") !== "false");
+  if (this.draggable_by_user) {
+    logDebug(this.id + "is draggable by user. Setting up draggable.");
     //set up our draggable:
-    this.draggable = Draggable (this.moving, { snap : function (x, y) {
-		        return sliders[this.slider_id].snap(x, y);
-		      }
+    this.draggable = Draggable(this.moving, { snap : function (x, y) {
+                        return sliders[this.slider_id].snap(x, y);
+                      }
     });
-    this.draggable.options.slider_id = this.id
+    this.draggable.options.slider_id = this.id;
     this.onchange = this.draggable.options.onchange;
   }
 
   // set to requested starting value:
-  this.set ( this.starting_value);
+  this.set(this.starting_value);
   return true;
 }
 
 Slider.prototype.val = function (x, y) {
   // returns the value, with respect to the slider, of a pair of mouse coordinates.
-  v = 0;
-  if (this.unit.x != 0) {
+  var v = 0;
+  if (this.unit.x !== 0) {
     v =  (x - this.start_offset.x - this.background_position.x) / this.unit.x;
   } else {
     v = (y - this.start_offset.y - this.background_position.y) / this.unit.y;
   }
-  if (v < this.min_value) return this.min_value
-  if (v > this.max_value) return this.max_value
+  if (v < this.min_value) { return this.min_value; }
+  if (v > this.max_value) { return this.max_value; }
   return v;
-}
+};
 
-Slider.prototype.pos = function(v) {
+Slider.prototype.pos = function (v) {
   // the position of the movable part of the slider when the slider is set to a given value.
-  x = this.start_offset.x +  this.background_position.x + this.unit.x * v
-  y = this.start_offset.y +  this.background_position.y + this.unit.y * v
-  return [x , y];
-}
+  var x = this.start_offset.x +  this.background_position.x + this.unit.x * v;
+  var y = this.start_offset.y +  this.background_position.y + this.unit.y * v;
+  return [x, y];
+};
 
 Slider.prototype.set = function (value) {
   // programatically set the slider at a given value.
-  p = this.pos (value);
-  setpos (this.moving, p[0], p[1]);
-}
+  var p = this.pos(value);
+  setpos(this.moving, p[0], p[1]);
+};
 
-Slider.prototype.get = function() {
+Slider.prototype.get = function () {
   // the current value of the slider
   return this.val(this.moving.x, this.moving.y);
-}
+};
 
-Slider.prototype.getfraction = function() {
+Slider.prototype.getfraction = function () {
   // the current value of the slider as a fraction of its max setting.
-  diff = this.max_value - this.min_value;
-  return (this.get() - this.min_value)/ diff;
-}
+  var diff = this.max_value - this.min_value;
+  return (this.get() - this.min_value) / diff;
+};
 
 
-Slider.prototype.snap = function(x, y) {
+Slider.prototype.snap = function (x, y) {
   // where to show it
   return this.pos(this.val(x, y));
-}
+};
 
-function pick_image (v, images) {
+function pick_image(v, images) {
   var media_path = $('media-base').getAttribute('data-image-base');
 
   n = images.length;
   //logDebug(images);
-  if (v != 0) {
-    a = Math.ceil(v * n ) - 1 ;
-    return media_path+images[a];
+  if (v !== 0) {
+    a = Math.ceil(v * n) - 1;
+    return media_path + images[a];
   }
   else {
-    logDebug ("yo");
+    logDebug("yo");
   }
-  return media_path+images[0];
+  return media_path + images[0];
 }
 
 /// Other common code:
 man_images = [
-  'images/man/xhosaman4.png'
-  ,'images/man/xhosaman3.png'
-  ,'images/man/xhosaman2.png'
-  ,'images/man/xhosaman1.png'
-  ,'images/man/xhosaman.png'
+  'images/man/xhosaman4.png',
+  'images/man/xhosaman3.png',
+  'images/man/xhosaman2.png',
+  'images/man/xhosaman1.png',
+  'images/man/xhosaman.png'
 ];
 
 
 woman_images = [
-  'images/woman/xhosawoman4.gif'
-  ,'images/woman/xhosawoman3.gif'
-  ,'images/woman/xhosawoman2.gif'
-  ,'images/woman/xhosawoman1.gif'
-  ,'images/woman/xhosawoman.gif'
+  'images/woman/xhosawoman4.gif',
+  'images/woman/xhosawoman3.gif',
+  'images/woman/xhosawoman2.gif',
+  'images/woman/xhosawoman1.gif',
+  'images/woman/xhosawoman.gif'
 ];
 
 default_state = {
-  'page_1_bad1' : null
- ,'page_1_bad2' : null
- ,'page_1_good' : null
- ,'page_2_good' : null
+  'page_1_bad1' : null,
+  'page_1_bad2' : null,
+  'page_1_good' : null,
+  'page_2_good' : null
 };
 
 var game_state;
 
-function clip_image (image, width, floor) {
+function clip_image(image, width, floor) {
   // sets the 'clip' style on an image so that any portion of the image below y value 'floor' is hidden.
   hide = floor  - elementPosition(image).y;
   setStyle(image, { 'clip': 'rect(0 ' + width + 'px ' + hide + 'px 0)' });
