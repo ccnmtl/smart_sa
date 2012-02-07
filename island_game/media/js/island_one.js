11807572d4668cade4645b015957687b833a9ad8
Anders Pearson <anders@columbia.edu>
2012-02-07 16:51:51 -0500

    follow symlinks to find javascript

--- island_game/media/js/island_one.js
+++ island_game/media/js/island_one.js
@@ -8,18 +8,18 @@ function init() {
     forEach (getElementsByTagAndClassName('span', 'slider'),
         function (a) { new Slider (a) }
     );
-    
+
     logDebug (serializeJSON(game_state));
     logDebug (game_state['page_1_bad1']);
-    
-    
+
+
     if (game_state['page_1_bad1'] != null) {
         logDebug ("yo");
         sliders['bad1'].set(game_state['page_1_bad1']);
         sliders['bad2'].set(game_state['page_1_bad2']);
         sliders['good'].set(game_state['page_1_good']);
     }
-    
+
     sliders['bad1'].draggable.options.onchange = recalc ;
     sliders['bad2'].draggable.options.onchange = recalc ;
     sliders['good'].draggable.options.onchange = recalc ;
@@ -41,17 +41,17 @@ function recalc() {
         sliders['bad1'].get() * sliders['bad1'].influence +
         sliders['bad2'].get() * sliders['bad2'].influence) /2;
     sliders['water'].set( water_level);
-    
+
     //adjust island level:
     island_level = (sliders['good'].get() * sliders['good'].influence );
     sliders['island'].set(island_level);
     island_height = sliders['island'].pos(sliders['island'].get())[1]
-    
+
     //pick an image for the dude:
     dude_images = (Intervention.current_user.gender == "M") ? man_images : woman_images;
     altitude = 0.5 + (sliders['island'].getfraction() - sliders['water'].getfraction())/2;
     $('dude').src = pick_image (altitude, dude_images);
-    
+
     //adjust the dude
     setStyle( 'dude', {
         "left":  ( $('dude').x  ) + 'px',
@@ -73,5 +73,5 @@ addLoadEvent(init);
 window.onresize= reloadme;
 function reloadme(){ //or whatver else you have
     save_state();
-    setTimeout("window.location.reload()",1); //
+    setTimeout(function () {window.location.reload();},1); //
 }
