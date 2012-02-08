2e8bae08d8f24a107df9bacc25ee20fc80d55fc5
Anders Pearson <anders@columbia.edu>
2012-02-08 16:12:15 -0500

    appeasing jshint

--- media/js/static_auth/test_session.js
+++ media/js/static_auth/test_session.js
@@ -7,55 +7,53 @@
  */
 
 
-function hasAttr(obj,key) {
-    try {
-        return (typeof(obj[key]) != 'undefined');
-    } catch(e) {return false;}
+function hasAttr(obj, key) {
+  try {
+    return (typeof(obj[key]) !== 'undefined');
+  } catch (e) { return false; }
 }
 
 /*wrap code with module pattern*/
-(function() {
-    var global = this;
-    var M = MochiKit.Base;
+(function () {
+  var global = this;
+  var M = MochiKit.Base;
 
-    function TestSession() {
-        this.permStor = global.globalStorage[location.hostname];
-        this.sessStor = global.sessionStorage;
-        this.nsUSER = 'USER_';
-        this.nsBACKUP = 'BACKUP_';
-        this.RESTORE_KEY = 'RESTORE';
-        if (!hasAttr(this.sessStor,this.nsUSER)) {
-            this.sessStor[this.nsUSER] = '{}';
-        }
+  function TestSession() {
+    this.permStor = global.globalStorage[location.hostname];
+    this.sessStor = global.sessionStorage;
+    this.nsUSER = 'USER_';
+    this.nsBACKUP = 'BACKUP_';
+    this.RESTORE_KEY = 'RESTORE';
+    if (!hasAttr(this.sessStor, this.nsUSER)) {
+      this.sessStor[this.nsUSER] = '{}';
     }
-    TestSession.prototype.currentUser = function() {
-        if (hasAttr(this.sessStor,this.nsUSER)) {
-            return M.evalJSON(this.sessStor[this.nsUSER].value);
-        } else {
-            this.sessStor[this.nsUSER] = '{}';
-            return {};
-        }
-    };
-    TestSession.prototype.saveUser = function(user_info,/*optional:*/ callback, user_key) {
-        this.sessStor[this.nsUSER] = M.serializeJSON(user_info);
-        if (callback) {
-            var response = callback(user_info);
-        }
-    };
+  }
+  TestSession.prototype.currentUser = function () {
+    if (hasAttr(this.sessStor, this.nsUSER)) {
+      return M.evalJSON(this.sessStor[this.nsUSER].value);
+    } else {
+      this.sessStor[this.nsUSER] = '{}';
+      return {};
+    }
+  };
+  TestSession.prototype.saveUser = function (user_info,/*optional:*/ callback, user_key) {
+    this.sessStor[this.nsUSER] = M.serializeJSON(user_info);
+    if (callback) {
+      var response = callback(user_info);
+    }
+  };
 
-    if (!hasAttr(global,'EphemeralSession')) {
-        if (hasAttr(global,'sessionStorage') && hasAttr(global,'globalStorage'))
-        {
-            if (location.hostname !== '') {
-                global.EphemeralSession = new TestSession();
-                //for easy debug access
-                global.e = global.EphemeralSession;
-            } else {
-                throw "We need a hostname, if on local file system, then go to the same url, but start it with file://localhost/ instead of file:/// after restarting the browser.";
-            }
-        }
-        else {
-            throw "No Firefox, and no support for other browsers (yet)!";
-        }
+  if (!hasAttr(global, 'EphemeralSession')) {
+    if (hasAttr(global, 'sessionStorage') && hasAttr(global, 'globalStorage')) {
+      if (location.hostname !== '') {
+        global.EphemeralSession = new TestSession();
+        //for easy debug access
+        global.e = global.EphemeralSession;
+      } else {
+        throw "We need a hostname, if on local file system, then go to the same url, but start it with file://localhost/ instead of file:/// after restarting the browser.";
+      }
+    } else {
+      throw "No Firefox, and no support for other browsers (yet)!";
     }
-})();
+  }
+}());
