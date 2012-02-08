fe5e829cbb3d1c3428d1af5bbce6a73c70ffb57f
Anders Pearson <anders@columbia.edu>
2012-02-08 09:32:27 -0500

    appeasing jshint

--- island_game/media/js/island_two.js
+++ island_game/media/js/island_two.js
@@ -16,54 +16,56 @@
 
       game_state = Intervention.getGameVar('island_game_state', default_state);
 
-      MI.forEach (MD.getElementsByTagAndClassName('span', 'slider'),
-        function (a) { new Slider (a) }
+      MI.forEach(MD.getElementsByTagAndClassName('span', 'slider'),
+        function (a) { new Slider(a); }
       );
 
-      if (game_state.page_2_good != null) {
-        sliders['good'].set( game_state.page_2_good);
+      if (game_state.page_2_good !== null) {
+        sliders.good.set(game_state.page_2_good);
       }
 
-      sliders['good'].draggable.options.onchange = recalc ;
-      mystery_factor = 20; // where does this come from?
+      sliders.good.draggable.options.onchange = recalc;
+      var mystery_factor = 20; // where does this come from?
       bottom_of_game = MD.elementDimensions(MD.getElement('sky')).h  + MD.elementPosition(MD.getElement('sky')).y - mystery_factor;
       recalc();
     }
 
 
     function save_state() {
-      game_state.page_2_good = sliders['good'].get();
+      game_state.page_2_good = sliders.good.get();
       Intervention.saveState();
     }
 
     function recalc() {
-      health = sliders['good'].get()
-      sliders['water'].set(10 - health);
-      sliders['bad1'].set( 10 - health);
-      sliders['bad2'].set( 10 - health);
-      sliders['dude'].set(health);
-      sliders['good2'].set( health);
+      health = sliders.good.get();
+      sliders.water.set(10 - health);
+      sliders.bad1.set(10 - health);
+      sliders.bad2.set(10 - health);
+      sliders.dude.set(health);
+      sliders.good2.set(health);
       MD.setStyle('dude_moving', {
-		    'height': '184px',
-		    'width': '110px'
-		  });
+        'height': '184px',
+        'width': '110px'
+      });
       //pick an image for the dude:
 
-      dude_images = (Intervention.current_user.gender == "M") ? man_images : woman_images;
-      MD.getElement('dude_moving').src = pick_image (sliders['dude'].getfraction(), dude_images);
+      dude_images = (Intervention.current_user.gender === "M") ? man_images : woman_images;
+      MD.getElement('dude_moving').src = pick_image(sliders.dude.getfraction(), dude_images);
 
-      clip_image (MD.getElement('water_moving'), 600, bottom_of_game);
+      clip_image(MD.getElement('water_moving'), 600, bottom_of_game);
     }
 
     MD.addLoadEvent(init);
 
     //this function snaps the graphics back into place on browser window resize
     //from http://forums.port80.asn.au/archive/index.php/t-8475.html
-    window.onresize= reloadme;
+
+    function reloadme() { //or whatver else you have
+      save_state();
+      setTimeout(function () { window.location.reload(); }, 1); //
+    }
+
+    window.onresize = reloadme;
     // connect(window, 'onbeforeunload', reloadme);
 
-    function reloadme(){ //or whatver else you have
-      save_state();
-      setTimeout("window.location.reload()",1); //
-    }
   })();
