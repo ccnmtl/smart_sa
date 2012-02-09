7ba0485bd0a897c554aee7ddefc54e7d15779976
Anders Pearson <anders@columbia.edu>
2012-02-09 14:52:19 -0500

    appeasing jshint (whitespace)

--- lifegoal_task/media/js/goals.js
+++ lifegoal_task/media/js/goals.js
@@ -1,27 +1,26 @@
-(function() {
-    var global = this;
-    var intervention = global.Intervention;
-    var goal_state = intervention.getGameVar('lifegoals', {});
+(function () {
+  var global = this;
+  var intervention = global.Intervention;
+  var goal_state = intervention.getGameVar('lifegoals', {});
 
-    var workform = null;
-    
-    function loadGoalTask() {
-	workform = $('lifegoals');
-	for (a in goal_state) {
-	    if (hasAttr(workform.elements,a)) {
-		workform.elements[a].value = goal_state[a];
-	    }
-	}
-	connect(workform,'onchange',saveForm);
-	connect(window,'onunload',saveForm);
+  var workform = null;
+
+  function loadGoalTask() {
+    workform = $('lifegoals');
+    for (a in goal_state) {
+      if (hasAttr(workform.elements, a)) {
+        workform.elements[a].value = goal_state[a];
+      }
     }
-    addLoadEvent(loadGoalTask);
+    connect(workform, 'onchange', saveForm);
+    connect(window, 'onunload', saveForm);
+  }
+  addLoadEvent(loadGoalTask);
 
-    function saveForm() {
-	forEach(workform.elements,function(elt) {
-	    goal_state[elt.name] = elt.value
-	});
-	intervention.saveState();
-    }
-
+  function saveForm() {
+    forEach(workform.elements, function (elt) {
+      goal_state[elt.name] = elt.value
+    });
+    intervention.saveState();
+  }
 })();
