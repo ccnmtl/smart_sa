577c516b010b361844b6a3111504fe0720c9665d
Anders Pearson <anders@columbia.edu>
2012-02-09 14:54:02 -0500

    appeasing jshint

--- lifegoal_task/media/js/goals.js
+++ lifegoal_task/media/js/goals.js
@@ -6,21 +6,21 @@
   var workform = null;
 
   function loadGoalTask() {
-    workform = $('lifegoals');
-    for (a in goal_state) {
+    workform = MochiKit.DOM.getElement('lifegoals');
+    for (var a in goal_state) {
       if (hasAttr(workform.elements, a)) {
         workform.elements[a].value = goal_state[a];
       }
     }
-    connect(workform, 'onchange', saveForm);
-    connect(window, 'onunload', saveForm);
+    MochiKit.Signals.connect(workform, 'onchange', saveForm);
+    MochiKit.Signals.connect(window, 'onunload', saveForm);
   }
-  addLoadEvent(loadGoalTask);
+  MochiKit.DOM.addLoadEvent(loadGoalTask);
 
   function saveForm() {
-    forEach(workform.elements, function (elt) {
-      goal_state[elt.name] = elt.value
+    MochiKit.Iter.forEach(workform.elements, function (elt) {
+      goal_state[elt.name] = elt.value;
     });
     intervention.saveState();
   }
-})();
+}());
