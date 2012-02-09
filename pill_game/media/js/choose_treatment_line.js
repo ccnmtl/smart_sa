bf3b6d64ae7d02d69fcf02e80a376689676108b0
Anders Pearson <anders@columbia.edu>
2012-02-09 11:40:42 -0500

    appeasing jshint and quarantining MochiKit

--- pill_game/media/js/choose_treatment_line.js
+++ pill_game/media/js/choose_treatment_line.js
@@ -1,35 +1,7 @@
 var game_state;
 
-addLoadEvent(init);
-
-function select_line(new_line) {
-  if (game_state['treatment_line']  != new_line) {
-    //logDebug ("Changing.");
-    clear_pill_info();
-  }
-  line_to_select = (new_line == 1 ? 'select_line_1' : 'select_line_2');
-  line_to_unselect =  (new_line == 1 ? 'select_line_2' : 'select_line_1');
-  addElementClass(line_to_select, 'line_selected');
-  removeElementClass(line_to_select, 'line_unselected');
-  addElementClass(line_to_unselect, 'line_unselected');
-  removeElementClass(line_to_unselect, 'line_selected');
-  game_state['treatment_line'] = new_line;
-  Intervention.saveState();
-}
-
-function init() {
-  game_state = Intervention.getGameVar('pill_game_state',  default_state);
-  if (game_state['treatment_line'] != "") {
-    select_line(game_state['treatment_line'])
-  }
-  connect('select_line_1', 'onclick', partial(select_line, 1));
-  connect('select_line_2', 'onclick', partial(select_line, 2));
-}
-
-
-
 function clear_pill_info() {
-  logDebug("CLEARING");
+  MochiKit.Logging.logDebug("CLEARING");
   // erase all pill info every time a client switches lines -- otherwise stuff gets confusing.
   game_state.selected_meds  = [];
   delete game_state.night_pills;
@@ -37,3 +9,31 @@ function clear_pill_info() {
   delete game_state.day_pills_time_menu_selected_index;
   delete game_state.night_pills_time_menu_selected_index;
 }
+
+
+function select_line(new_line) {
+  if (game_state.treatment_line  !== new_line) {
+    //logDebug ("Changing.");
+    clear_pill_info();
+  }
+  var line_to_select = (new_line === 1 ? 'select_line_1' : 'select_line_2');
+  var line_to_unselect = (new_line === 1 ? 'select_line_2' : 'select_line_1');
+  MochiKit.DOM.addElementClass(line_to_select, 'line_selected');
+  MochiKit.DOM.removeElementClass(line_to_select, 'line_unselected');
+  MochiKit.DOM.addElementClass(line_to_unselect, 'line_unselected');
+  MochiKit.DOM.removeElementClass(line_to_unselect, 'line_selected');
+  game_state.treatment_line = new_line;
+  Intervention.saveState();
+}
+
+function init() {
+  game_state = Intervention.getGameVar('pill_game_state',  default_state);
+  if (game_state.treatment_line !== "") {
+    select_line(game_state.treatment_line);
+  }
+  MochiKit.Signals.connect('select_line_1', 'onclick', MochiKit.Base.partial(select_line, 1));
+  MochiKit.Signals.connect('select_line_2', 'onclick', MochiKit.Base.partial(select_line, 2));
+}
+
+
+MochiKit.DOM.addLoadEvent(init);
