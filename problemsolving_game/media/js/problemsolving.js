6c3df0921a7230add3336d2f1b0d8cd5ed59c70c
Anders Pearson <anders@columbia.edu>
2012-02-09 14:29:17 -0500

    appeasing jshint

--- problemsolving_game/media/js/problemsolving.js
+++ problemsolving_game/media/js/problemsolving.js
@@ -23,25 +23,29 @@
     this.intervention = global.Intervention;
     this.game_state = this.intervention.getGameVar('problemsolving', { 'my_issues': {} });
     connect(window, 'onload', this, 'onLoad');
-  }
+  };
   ProblemSolveGame.prototype.onLoad = function () {
     var self = this;
     var mode = getElement('gamephase').className;
 
     var start_at_choose_one = false;
     //which ones picked
-    for (a in self.game_state.my_issues) {
+    for (var a in self.game_state.my_issues) {
       var issue = self.getIssueByText(a);
       if (issue) {
-        addElementClass(issue,'picked');
-        if (mode == 'choose_one') {
+        addElementClass(issue, 'picked');
+        if (mode === 'choose_one') {
           start_at_choose_one = true;
           var filled_out_form = false;
-          for (b in self.game_state.my_issues[a]) {
+          for (var b in self.game_state.my_issues[a]) {
             filled_out_form = true;
             break;//not empty dict
           }
-          (filled_out_form) ? addElementClass(issue, 'completed') : removeElementClass(issue, 'completed');
+          if (filled_out_form) {
+            addElementClass(issue, 'completed');
+          } else {
+            removeElementClass(issue, 'completed');
+          }
           connect(issue, 'onclick', bind(self.chooseOneIssue, self, null, issue, filled_out_form));
         }
       }
@@ -51,15 +55,15 @@
       self.intervention.saveState();
     }
 
-    if (mode == 'my_issues') {
+    if (mode === 'my_issues') {
       $('next-game-part-link').onclick = function () {
         if (!self.anyIssues()) {
           alert('Please go back and select at least one issue');
           return false;
         }
-      }
+      };
     }
-    if (mode == 'choose_one') {
+    if (mode === 'choose_one') {
       if (!hasAttr(self.game_state, 'chosen-issue')) {
         $('next-game-part-link').onclick = function () {
           if (!self.game_state['chosen-issue']) {
@@ -70,11 +74,11 @@
       }
     }
 
-    if (mode == 'problemsolve_one') {
-      var workthrough_form = document.forms['workthrough_form'];
+    if (mode === 'problemsolve_one') {
+      var workthrough_form = document.forms.workthrough_form;
       if (hasAttr(self.game_state, 'chosen-issue')) {
-        var problemsolve_state = self.game_state['my_issues'][self.game_state['chosen-issue']];
-        for (field in problemsolve_state) {
+        var problemsolve_state = self.game_state.my_issues[self.game_state['chosen-issue']];
+        for (var field in problemsolve_state) {
           workthrough_form.elements[field].value = problemsolve_state[field];
         }
       }
@@ -104,16 +108,16 @@
     }
 
     if (hasAttr(self.game_state, 'chosen-issue')) {
-      var issue = self.getIssueByText(self.game_state['chosen-issue']);
-      self.chooseOneIssue(null, issue);
+      var issue2 = self.getIssueByText(self.game_state['chosen-issue']);
+      self.chooseOneIssue(null, issue2);
     }
-  }
+  };
   ProblemSolveGame.prototype.previousIssue = function () {
     var cur_issue = getFirstElementByTagAndClassName(null, 'currentissue', 'issue-list');
     function getPreviousIssue(cur_issue) {
       var prev_issue = cur_issue.previousSibling;
-      while (prev_issue != null) {
-        if (prev_issue.nodeType == 1) {
+      while (prev_issue !== null) {
+        if (prev_issue.nodeType === 1) {
           break;//we found the next issue
         }
         prev_issue = prev_issue.previousSibling;
@@ -121,17 +125,17 @@
       return prev_issue;
     }
     var prev_issue = getPreviousIssue(cur_issue);
-    if (prev_issue != null) {
+    if (prev_issue !== null) {
       removeElementClass(cur_issue, 'currentissue');
       addElementClass(prev_issue, 'currentissue');
-      if (getPreviousIssue(prev_issue) == null) {
+      if (getPreviousIssue(prev_issue) === null) {
         hideElement('prev-issue-link');
       }
     } else {
       hideElement('prev-issue-link');
     }
     return false;
-  }
+  };
 
   ProblemSolveGame.prototype.answerIssue = function (is_an_issue) {
     var self = this;
@@ -140,29 +144,29 @@
     var issue_text = self.getIssueText(cur_issue);
     if (is_an_issue && issue_text) {
       //if already there, leave it alone:
-      if (!hasAttr(self.game_state['my_issues'], issue_text)) {
-        self.game_state['my_issues'][issue_text] = {};
+      if (!hasAttr(self.game_state.my_issues, issue_text)) {
+        self.game_state.my_issues[issue_text] = {};
       }
     } else {
-      delete self.game_state['my_issues'][issue_text];
+      delete self.game_state.my_issues[issue_text];
     }
     self.intervention.saveState();
 
     var next_issue = cur_issue.nextSibling;
-    while (next_issue != null) {
-      if (next_issue.nodeType == 1) {
+    while (next_issue !== null) {
+      if (next_issue.nodeType === 1) {
         break;//we found the next issue
       }
       next_issue = next_issue.nextSibling;
     }
-    if (next_issue != null) {
+    if (next_issue !== null) {
       removeElementClass(cur_issue, 'currentissue');
       addElementClass(next_issue, 'currentissue');
       showElement('prev-issue-link');
     } else {
       location.assign(getElement("next-game-part-link").href);
     }
-  }
+  };
 
   ProblemSolveGame.prototype.getIssueByText = function (text) {
     var self = this;
@@ -170,7 +174,7 @@
     var other = getFirstElementByTagAndClassName(null, 'editable-issue', 'issue-list');
     var rv = false;
     forEach(issues, function (elt) {
-      if (self.getIssueText(elt) == text) {
+      if (self.getIssueText(elt) === text) {
         rv = elt;
       }
     });
@@ -186,18 +190,18 @@
       rv = other;
     }
     return rv;
-  }
+  };
   ProblemSolveGame.prototype.anyIssues = function () {
-    for (a in this.game_state.my_issues) {
+    for (var a in this.game_state.my_issues) {
       return true;
     }
     return false;
-  }
+  };
   ProblemSolveGame.prototype.getIssueText = function (issue) {
     var text_dom = getFirstElementByTagAndClassName(null, 'issuetext', issue);
     var issue_text = (String(text_dom.tagName).toLowerCase() in {'input': 1, 'textarea': 1}) ? text_dom.value : text_dom.innerHTML;
     return issue_text;
-  }
+  };
   ProblemSolveGame.prototype.nextField = function (evt) {
     var hash = String(global.location.hash).substr(1);
     var index = findValue(this.field_phases, hash);
@@ -218,12 +222,16 @@
      video_embed.SetControllerVisible(true);
      */
 
-    (hash == 'gamephase') ? addElementClass('gamephase', 'general') : removeElementClass('gamephase', 'general');
+    if (hash === 'gamephase') {
+      addElementClass('gamephase', 'general');
+    } else {
+      removeElementClass('gamephase', 'general');
+    }
     this.resizeTextAreas();
-  }
+  };
 
   ProblemSolveGame.prototype.resizeTextAreas = function () {
-    if (!hasElementClass('gamephase', 'general')) return;
+    if (!hasElementClass('gamephase', 'general')) { return; }
     forEach(getElementsByTagAndClassName('textarea'), function (src) {
       //var src = evt.src();
       var lines_array = String(src.value).match(/\n/g);
@@ -231,16 +239,16 @@
         src.rows = lines_array.length + 1;
       }
     });
-  }
+  };
   ProblemSolveGame.prototype.saveProblemSolveForm = function (evt) {
     var self = this;
-    var problemsolve_state = self.game_state['my_issues'][self.game_state['chosen-issue']];
+    var problemsolve_state = self.game_state.my_issues[self.game_state['chosen-issue']];
 
-    forEach(document.forms['workthrough_form'].elements, function (elt) {
+    forEach(document.forms.workthrough_form.elements, function (elt) {
               problemsolve_state[elt.name] = elt.value;
             });
     self.intervention.saveState();
-  }
+  };
   ProblemSolveGame.prototype.chooseOneIssue = function (evt, issue, immediately_advance) {
     var self = this;
     issue = (issue) ? issue : evt.src();
@@ -258,8 +266,8 @@
     if (immediately_advance) {
       global.location = $('next-game-part-link').href + "#gamephase";
     }
-  }
+  };
 
   global.ProblemSolveGame = new ProblemSolveGame();
-})();
+}());
 
