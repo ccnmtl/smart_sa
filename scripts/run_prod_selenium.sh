41947f5243374a17e54d6c3f5bc700aa3af54793
Anders Pearson <anders@columbia.edu>
2012-02-20 17:00:48 -0500

    prod version

---
+++ scripts/run_prod_selenium.sh
@@ -0,0 +1,1 @@
+rm -rf ~/clean_chrome && chromium-browser --user-data-dir=~/clean_chrome http://masivukeni2.ccnmtl.columbia.edu/site_media/selenium/TestRunner.html\?test\=..%2Ftests%2FTestSuite.html\&resultsUrl\=..%2FpostResults
