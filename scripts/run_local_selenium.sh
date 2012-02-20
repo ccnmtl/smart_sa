01f7b8f85ac2a152872ccbe45519dbedaa588102
Anders Pearson <anders@columbia.edu>
2012-02-20 16:59:09 -0500

    script for running selenium tests in a clean chromium profile (nothing cached)

---
+++ scripts/run_local_selenium.sh
@@ -0,0 +1,1 @@
+rm -rf ~/clean_chrome && chromium-browser --user-data-dir=~/clean_chrome http://localhost:8000/site_media/selenium/TestRunner.html\?test\=..%2Ftests%2FTestSuite.html\&resultsUrl\=..%2FpostResults
