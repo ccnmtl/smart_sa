6598233722dd675bd35803f10642fd4ced0f1fde
Anders Pearson <anders@columbia.edu>
2012-02-22 14:51:15 -0500

    work off a clean base (where i've gone through some of the annoying setup dialogs) which gets to a temp profile on each run

--- scripts/run_local_selenium.sh
+++ scripts/run_local_selenium.sh
@@ -1,1 +1,4 @@
-rm -rf ~/clean_chrome && chromium-browser --user-data-dir=~/clean_chrome http://localhost:8000/site_media/selenium/TestRunner.html\?test\=..%2Ftests%2FTestSuite.html\&resultsUrl\=..%2FpostResults
+#!/bin/bash
+rm -rf $HOME/tmp_chrome
+cp -R $HOME/clean_chrome $HOME/tmp_chrome
+chromium-browser --user-data-dir=$HOME/tmp_chrome http://localhost:8000/site_media/selenium/TestRunner.html\?test\=..%2Ftests%2FTestSuite.html\&resultsUrl\=..%2FpostResults
--- scripts/run_prod_selenium.sh
+++ scripts/run_prod_selenium.sh
@@ -1,1 +1,5 @@
-rm -rf ~/clean_chrome && chromium-browser --user-data-dir=~/clean_chrome http://masivukeni2.ccnmtl.columbia.edu/site_media/selenium/TestRunner.html\?test\=..%2Ftests%2FTestSuite.html\&resultsUrl\=..%2FpostResults
+#!/bin/bash
+rm -rf $HOME/tmp_chrome
+cp -R $HOME/clean_chrome $HOME/tmp_chrome
+chromium-browser --user-data-dir=$HOME/tmp_chrome http://masivukeni2.ccnmtl.columbia.edu/site_media/selenium/TestRunner.html\?test\=..%2Ftests%2FTestSuite.html\&resultsUrl\=..%2FpostResults
+
