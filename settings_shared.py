89e0dec5ed388101fc1264f5533ecd5231ead782
Anders Pearson <anders@columbia.edu>
2012-02-24 10:58:28 -0500

    need a default set for that apparently

--- settings_shared.py
+++ settings_shared.py
@@ -124,6 +124,8 @@ TEMPLATE_CONTEXT_PROCESSORS = ("django.core.context_processors.auth",
 
 PROD_BASE_URL = "http://masivukeni2.ccnmtl.columbia.edu/"
 PROD_MEDIA_BASE_URL = "http://masivukeni2.ccnmtl.columbia.edu/multimedia/"
+
+DISABLE_OFFLINE = False
  
 try:
     from release_id import LAST_GIT_HEAD
