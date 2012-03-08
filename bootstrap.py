e9e8e28c0f5c3466216bf5e4e9d7f4b23d49fc97
Anders Pearson <anders@columbia.edu>
2012-03-08 15:30:28 -0500

    eggs are gone. we can remove that

--- bootstrap.py
+++ bootstrap.py
@@ -26,19 +26,6 @@ ret = subprocess.call([os.path.join(vedir, 'bin', 'pip'), "install",
                        "--requirement",os.path.join(pwd,"requirements/apps.txt")])
 if ret: exit(ret)
 
-def has_eggs():
-    return [os.path.basename(path) for path in
-            glob.glob(os.path.join(pwd, "requirements", "eggs", "*.egg"))]
-                  
-
-if has_eggs():
-    # only try to easy install eggs if there actually are some
-    cmd = ([os.path.join(vedir,"bin/easy_install"),
-            '-f', os.path.join(pwd,"requirements/eggs/")] +
-           has_eggs())
-    ret = subprocess.call(cmd)
-    exit(ret)
-
 ret = subprocess.call(["python","virtualenv.py","--relocatable",vedir])
 # --relocatable always complains about activate.csh, which we don't really
 # care about. but it means we need to ignore its error messages
