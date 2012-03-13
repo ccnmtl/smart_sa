f650ca08be4fff5feed006e9ca56a1d56e8efc3a
sdreher <sdreher@gmail.com>
2012-03-13 19:35:13 -0400

    * Island Game folded into one screen.

--- island_game/media/js/islandview.js
+++ island_game/media/js/islandview.js
@@ -5,71 +5,35 @@
     var GameElement = Backbone.Model.extend({
         defaults: {
             name: "",
-            influence: 0,
             clip_floor: 0,
             min_value : 0,
-            max_value : 0,
+            max_value : 10,
             starting_value : 0,
             start_offset: { x: 0, y: 0 },
             end_offset: { x: 0, y: 0 },
-            draggable: false
+            draggable: false,
+            gender: "",
+            enabled: true
         },
         
         initialize : function (options) {
+            
+        },
+        
+        unit: function () {
             var range = this.get("max_value") - this.get("min_value");
-            this.set("unit", {
+            return {
                 x: (this.get("end_offset").x - this.get("start_offset").x) / range,
                 y: (this.get("end_offset").y - this.get("start_offset").y) / range
-            });
+            };
         }
     });
     
     var GameElementList = Backbone.Collection.extend({ model : GameElement });
 
-    
-    var FigureView = Backbone.View.extend({
-        initialize : function (options) {
-            _.bindAll(this, "selectFigure", "update");
-            var self = this;
-        },
-        
-        male_images: [
-            'man/xhosaman4.png',
-            'man/xhosaman3.png',
-            'man/xhosaman2.png',
-            'man/xhosaman1.png',
-            'man/xhosaman.png'
-        ],
-        
-        female_images: [
-            'woman/xhosawoman4.gif',
-            'woman/xhosawoman3.gif',
-            'woman/xhosawoman2.gif',
-            'woman/xhosawoman1.gif',
-            'woman/xhosawoman.gif'
-        ],
-                  
-        media_path: "/site_media/island_game/images/",
-        
-        selectFigure: function (altitude) {
-            var images = (this.gender === "M") ? this.male_images : this.female_images;
-          
-            if (altitude !== 0) {
-                var i = Math.ceil(altitude * images.length) - 1;
-                this.el.src = this.media_path + images[i];
-            }
-        },
-        
-        update: function (altitude, top) {
-            this.selectFigure(altitude);
-            jQuery(this.el).css("top", top + jQuery(this.el).height() / 4);
-        }
-    });
-
-    
     var SliderView = Backbone.View.extend({
         initialize : function (options) {
-            _.bindAll(this, "val", "pos", "setValue", "getValue", "getfraction", "snap", "top");
+            _.bindAll(this, "val", "pos", "setValue", "getValue", "getfraction", "snap", "top", "render");
             var self = this;
             
             this.parent = options.parent;
@@ -77,24 +41,49 @@
             if (this.model.get("draggable")) {
                 this.draggable = M.DragAndDrop.Draggable(this.el, {
                     snap : function (x, y) {
-                        return self.snap(x, y);
+                        if (self.model.get("enabled")) {
+                            return self.snap(x, y);
+                        }
                     }
                 });
                 this.draggable.options.slider_id = this.el.id;
-                this.draggable.options.onchange = function () { self.parent.trigger("recalc"); };
+                this.draggable.options.onchange = function () {
+                    if (self.model.get("enabled")) {
+                        self.parent.trigger("render");
+                    }
+                };
             }
 
             // set to requested starting value:
             this.setValue(this.model.get("starting_value"));
+            
+            this.model.bind('change:enabled', this.render);
+        },
+        
+        disable: function () {
+            this.model.set("enabled", false);
+        },
+        
+        enable: function () {
+            this.model.set("enabled", true);
+        },
+        
+        render: function () {
+            if (this.model.get("enabled")) {
+                jQuery("." + this.model.get("name")).css('opacity', 1);
+            } else {
+                jQuery("." + this.model.get("name")).css('opacity', 0.5);
+            }
         },
         
         val: function (x, y) {
             // returns the value, with respect to the slider, of a pair of mouse coordinates.
             var v = 0;
-            if (this.model.get("unit").x !== 0) {
-                v =  (x - this.model.get("start_offset").x) / this.model.get("unit").x;
+            var unit = this.model.unit();
+            if (unit.x !== 0) {
+                v =  (x - this.model.get("start_offset").x) / unit.x;
             } else {
-                v = (y - this.model.get("start_offset").y) / this.model.get("unit").y;
+                v = (y - this.model.get("start_offset").y) / unit.y;
             }
             if (v < this.model.get("min_value")) {
                 return this.model.get("min_value");
@@ -107,8 +96,9 @@
 
         pos: function (v) {
             // the position of the movable part of the slider when the slider is set to a given value.
-            var x = this.model.get("start_offset").x + this.model.get("unit").x * v;
-            var y = this.model.get("start_offset").y + this.model.get("unit").y * v;
+            var unit = this.model.unit();
+            var x = this.model.get("start_offset").x + unit.x * v;
+            var y = this.model.get("start_offset").y + unit.y * v;
             return [x, y];
         },
 
@@ -156,29 +146,90 @@
         }
     });
     
+    var FigureView = SliderView.extend({
+        initialize : function (options) {
+            _.bindAll(this, "update", "setValue");
+            var self = this;
+        },
+        
+        male_images: [
+            'man/xhosaman4.png',
+            'man/xhosaman3.png',
+            'man/xhosaman2.png',
+            'man/xhosaman1.png',
+            'man/xhosaman.png'
+        ],
+        
+        female_images: [
+            'woman/xhosawoman4.gif',
+            'woman/xhosawoman3.gif',
+            'woman/xhosawoman2.gif',
+            'woman/xhosawoman1.gif',
+            'woman/xhosawoman.gif'
+        ],
+                  
+        media_path: "/site_media/island_game/images/",
+        
+        images: function () {
+            return (this.model.get("gender") === "M") ? this.male_images : this.female_images;
+        },
+        
+        selectImage: function (value) {
+            var images = this.images();
+            
+            if (value === 0) {
+                this.el.src = this.media_path + images[0];
+            } else {
+                var i = Math.ceil(value * images.length) - 1;
+                this.el.src = this.media_path + images[i];
+            }
+        },
+        
+        setValue: function (value) {
+            SliderView.prototype.setValue.call(this, value);
+            this.selectImage(SliderView.prototype.getfraction.call(this));
+        },
+        
+        update: function (altitude, top) {
+            this.selectImage(altitude);
+            jQuery(this.el).css("top", top + jQuery(this.el).height() / 4);
+        }
+    });
+    
+    var IslandGame = Backbone.Model.extend({
+        defaults: {
+            beforeMedication: true
+        },
+        
+        initialize : function (options) {
+        }
+    });
+    
     
     var IslandGameView = Backbone.View.extend({
         events: {
+            'click img#right' : 'afterMedicationView',
+            'click img#left': 'beforeMedicationView'
         },
-
+        
         initialize : function (options) {
-            _.bindAll(this, "recalc", "gameFloor", "addGameElement", "waterLevel");
+            _.bindAll(this, "render", "gameFloor", "addGameElement", "waterLevel", 'afterMedicationView', 'beforeMedicationView');
             _.extend(this, Backbone.Events);
+            this.on("render", this.render);
             
             this.views = {};
-            this.gender = options.gender;
             this.collection = options.collection;
             this.collection.bind('add', this.addGameElement);
-            
-            this.on("recalc", this.recalc);
+            this.model.bind('change', this.render);
         },
         
         addGameElement: function (element) {
             var name = element.get("name");
             if (name === 'figure') {
+                element.set("gender", this.model.get("gender"));
                 this.views[name] = new FigureView({ model: element, el: jQuery("#" + name) });
             } else {
-                this.views[name] = new SliderView({ model: element, el: jQuery("#" + name), parent: this }).render();
+                this.views[name] = new SliderView({ model: element, el: jQuery("#" + name), parent: this });
             }
         },
         
@@ -191,15 +242,83 @@
             return (this.views.infection.getValue() + this.views.viral_load.getValue()) / 2;
         },
         
-        recalc: function (msg) {
-            this.views.water.setValue(this.waterLevel());
-
-            //adjust island level:
-            var island_level = this.views.cd4_count.getValue();
-            this.views.island.setValue(island_level);
+        beforeMedicationView: function () {
+            jQuery("img#right").show();
+            jQuery("img#left").hide();
+            jQuery("span#island_view_label").html("BEFORE GOING ON ARVS");
+            jQuery("img#island").attr("src", "/site_media/island_game/images/island_part1.png");
             
-            var altitude = 0.5 + (this.views.island.getfraction() - this.views.water.getfraction()) / 2;
-            this.views.figure.update(altitude, this.views.island.top());
+            jQuery("#adherence_label").hide();
+            jQuery("#adherence_slider").hide();
+            jQuery("#adherence").hide();
+            
+            jQuery("#figure").addClass("figure_one");
+            jQuery("#figure").removeClass("figure_two");
+            
+            this.views.infection.enable();
+            this.views.viral_load.enable();
+            this.views.cd4_count.enable();
+            
+            var start_offset = { x: 21, y: 300 };
+            var end_offset = { x: 21, y: 100 };
+            this.views.figure.model.set("start_offset", start_offset);
+            this.views.figure.model.set("end_offset", end_offset);
+            
+            this.views.infection.setValue(0);
+            this.views.viral_load.setValue(0);
+            this.views.cd4_count.setValue(10);
+            this.model.set("beforeMedication", true);
+        },
+        
+        afterMedicationView: function () {
+            jQuery("img#right").hide();
+            jQuery("img#left").show();
+            jQuery("span#island_view_label").html("ON ARVS");
+            jQuery("img#island").attr("src", "/site_media/island_game/images/island_part2.png");
+            
+            jQuery("#adherence_label").show();
+            jQuery("#adherence_slider").show();
+            jQuery("#adherence").show();
+            
+            jQuery("#figure").removeClass("figure_one");
+            jQuery("#figure").addClass("figure_two");
+            
+            var start_offset = { x: 150, y: 200 };
+            var end_offset = { x: 400, y: 125 };
+            this.views.figure.model.set("start_offset", start_offset);
+            this.views.figure.model.set("end_offset", end_offset);
+
+            this.views.infection.disable();
+            this.views.viral_load.disable();
+            this.views.cd4_count.disable();
+            
+            this.views.adherence.setValue(10);
+            this.views.infection.setValue(0);
+            this.views.viral_load.setValue(0);
+            this.views.cd4_count.setValue(10);
+
+            this.model.set("beforeMedication", false);
+        },
+        
+        render: function () {
+            if (this.model.get("beforeMedication")) {
+                this.views.water.setValue(this.waterLevel());
+    
+                var island_level = this.views.cd4_count.getValue();
+                this.views.island.setValue(island_level);
+                
+                var altitude = 0.5 + (this.views.island.getfraction() - this.views.water.getfraction()) / 2;
+                this.views.figure.update(altitude, this.views.island.top());
+            } else {
+                this.views.island.setValue(10);
+                
+                var health = this.views.adherence.getValue();
+                this.views.water.setValue(10 - health);
+                this.views.infection.setValue(10 - health);
+                this.views.viral_load.setValue(10 - health);
+                this.views.cd4_count.setValue(health);
+                this.views.figure.setValue(health);
+            }
         }
     });
     
@@ -210,25 +329,20 @@
     jQuery(document).ready(function () {
         var elements = new GameElementList();
         var islandView = new IslandGameView({
+            model: new IslandGame(),
+            collection: elements,
             el: 'div#island_container',
-            gender: global.Intervention.current_user.gender,
-            collection: elements
         });
         
         var model = new GameElement({
             name: "figure",
-            min_value: 0,
-            max_value: 10,
             starting_value: 10,
-            start_offset: { x: 21, y: 300 },
-            end_offset: { x: 21, y: 100 }
+            gender: global.Intervention.current_user.gender
         });
         elements.add(model);
         
         model = new GameElement({
             name: "island",
-            min_value: 0,
-            max_value: 10,
             starting_value: 10,
             start_offset: { x: 21, y: 300 },
             end_offset: { x: 21, y: 100 },
@@ -238,20 +352,15 @@
         
         model = new GameElement({
             name: "water",
-            min_value: 0,
-            max_value: 10,
             starting_value: 0,
             start_offset: { x: 21, y: 400 },
-            end_offset: { x: 21, y: 200 },
+            end_offset: { x: 21, y: 175 },
             clip_floor: islandView.gameFloor()
         });
         elements.add(model);
         
         model = new GameElement({
             name: "infection",
-            influence: 1.0,
-            min_value: 0,
-            max_value: 10,
             starting_value: 0,
             start_offset: { x: 50, y: 245 },
             end_offset: { x: 50, y: 154 },
@@ -261,10 +370,7 @@
                     
         model = new GameElement({
             name: "viral_load",
-            influence: 1.0,
             starting_value: 0,
-            min_value: 0,
-            max_value: 10,
             start_offset: { x: 100, y: 245 },
             end_offset: { x: 100, y: 154 },
             draggable: true
@@ -273,9 +379,6 @@
         
         model = new GameElement({
             name: "cd4_count",
-            influence: 1.0,
-            min_value: 0,
-            max_value: 10,
             starting_value: 10,
             start_offset: { x: 150, y: 245 },
             end_offset: { x: 150, y: 154 },
@@ -285,8 +388,6 @@
         
         model = new GameElement({
             name: "adherence",
-            min_value: 0,
-            max_value: 10,
             starting_value: 10,
             start_offset: { x: 500, y: 245 },
             end_offset: { x: 500, y: 154 },
@@ -294,7 +395,6 @@
         });
         elements.add(model);
         
-        islandView.trigger("recalc");
-        
+        islandView.trigger("render");
     });
 }(jQuery));
