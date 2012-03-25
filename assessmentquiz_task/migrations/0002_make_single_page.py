8573ea919e1e2a196b7fdd90d73676f3a84db145
sdreher <sdreher@gmail.com>
2012-03-25 09:39:40 -0400

    * Splitting the assessment quiz into 2 distinct activities.

---
+++ assessmentquiz_task/migrations/0002_make_single_page.py
@@ -0,0 +1,21 @@
+# encoding: utf-8
+from south.v2 import DataMigration
+from intervention.models import Activity, GamePage
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        activities = Activity.objects.filter(game='assessmentquiz')
+        for a in activities:
+            GamePage.objects.filter(activity=a, _order__gte=1).delete()
+
+
+    def backwards(self, orm):
+        "Write your backwards methods here."
+
+
+    models = {
+        
+    }
+
+    complete_apps = ['assessmentquiz_task']
--- assessmentquiz_task/models.py
+++ assessmentquiz_task/models.py
@@ -4,15 +4,19 @@ from django.db import models
 from intervention.installed_games import InstalledGames,GameInterface
 
 class AssessmentQuizTask(GameInterface):
+    def __init__(self, mode):
+        self.mode = mode 
+        
     def pages(self):
-        return ('kten','audit')
+        return ('quiz',)
 
     def template(self,page_id):
-        game_context = {'mode':page_id,}
-        return ('assessmentquiz_task/%s.html' % page_id ,game_context)
+        game_context = {'mode': self.mode}
+        return ('assessmentquiz_task/%s.html' % self.mode ,game_context)
 
     def variables(self,page_id=None):
         return ['assessmentquiz']
 
-InstalledGames.register_game('assessmentquiz','Assessment Quiz', AssessmentQuizTask() )
+InstalledGames.register_game('assessmentquiz','Assessment Quiz Mood', AssessmentQuizTask('kten'))
+InstalledGames.register_game('assessmentquiz-audit','Assessment Quiz Audit', AssessmentQuizTask('audit'))
 
