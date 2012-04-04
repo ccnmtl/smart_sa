3797a3070eaddc7770ec0444b45bd7c325cfbda0
sdreher <sdreher@gmail.com>
2012-04-03 21:02:21 -0400

    Pill game progress.

--- pill_game/media/js/pillview.js
+++ pill_game/media/js/pillview.js
@@ -2,43 +2,127 @@
     var global = this;
     var M = MochiKit;
     
+    global.dropped = false;
+    global.golden_ratio_conjugate = 0.618033988749895;
+    global.h = 0;
+    
+    function getHue() {
+        if (global.h === 0)
+            global.h = Math.random();
+        global.h += golden_ratio_conjugate;
+        global.h %= 1;
+        return global.h;
+    }
+    
     var Pill = Backbone.Model.extend({
         defaults: {
-            name: "" 
+            id: null,
+            text: "",
+            ordinal: 0,
+            mode: '',
+            color: null
+        },
+
+        /**
+         * http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+         * Converts an HSV color value to RGB. Conversion formula
+         * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+         * Assumes h, s, and v are contained in the set [0, 1] and
+         * returns r, g, and b in the set [0, 255].
+         *
+         * @param   Number  h       The hue
+         * @param   Number  s       The saturation
+         * @param   Number  v       The value
+         * @return  Array           The RGB representation
+         */
+        hsvToRgb: function(h, s, v){
+            var r, g, b;
+
+            var i = Math.floor(h * 6);
+            var f = h * 6 - i;
+            var p = v * (1 - s);
+            var q = v * (1 - f * s);
+            var t = v * (1 - (1 - f) * s);
+
+            switch(i % 6){
+                case 0: r = v; g = t; b = p; break;
+                case 1: r = q; g = v; b = p; break;
+                case 2: r = p; g = v; b = t; break;
+                case 3: r = p; g = q; b = v; break;
+                case 4: r = t; g = p; b = v; break;
+                case 5: r = v; g = p; b = q; break;
+            }
+
+            return [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255)   ];
+        },
+        initialize: function() {
+            if (this.get("color") === null) {
+                var a = this.hsvToRgb(getHue(), 0.99, 0.99);
+                this.set("color", "rgb(" + a[0] + "," + a[1] + "," + a[2] + ")");
+            }
         }
     });
     
-    var PillList = Backbone.Collection.extend({ model : Pill });
+    var PillList = Backbone.Collection.extend({ 
+        model : Pill,
+        practiceList: function () {
+            this.add(new Pill({ id: 'd4t', text: 'D4T<br />2 pills twice a day', ordinal: 1, mode: 'practice', color: "red" }));
+            this.add(new Pill({ id: '3tc', text: '3TC<br />2 pills twice a day', ordinal: 2, mode: 'practice', color: "blue" }));
+            this.add(new Pill({ id: 'efavirenz', text: 'Efavirenz<br />1 pill in the evening', ordinal: 3, mode: 'practice', color: "green" }));
+        }
+    });
     
-    var PillView = Backbone.View.extend({
-        initialize: function (options) {
-            _.bindAll(this);
-            this.parent = options.parent;
-            this.ordinal = options.ordinal;
-            this.dropped = false;
+    var DraggablePillView = Backbone.View.extend({
+        initialize: function (options, render) {
+            _.bindAll(this, "revertEffect");
             this.draggable = new Draggable(this.el, { 
                 revert: true,
-                reverteffect: this.parent.revertEffect
+                reverteffect: this.revertEffect
             });
         },
-        
-        render: function () {
+        revertEffect: function (innerelement, top_offset, left_offset) {
+            var dur = 0;
+            if (!global.dropped) {
+               dur = Math.sqrt(Math.abs(top_offset ^ 2) + Math.abs(left_offset ^ 2)) * 0.02;
+            }
+            
+            global.dropped = false;
+            return new (MochiKit.Visual.Move)(innerelement, {x: - left_offset, y: - top_offset, duration: dur});
         }
     });
     
-    var Bucket = Backbone.Model.extend({
-        defaults: {
-            type: "" // day or night
+    var TrashablePillView = Backbone.View.extend({
+        initialize: function (options, render) {
+            _.bindAll(this, "revertEffect");
+            this.parent = options.parent;
+            this.draggable = new Draggable(this.el, { 
+                revert: true,
+                reverteffect: this.revertEffect
+            });
+        },
+        revertEffect: function (innerelement, top_offset, left_offset) {
+            var rv;
+            if (jQuery(innerelement).hasClass("trashable")) {
+                if (!global.dropped) {
+                    this.parent.trigger("trashPill", innerelement.id); // delete me
+                }
+            }
+            global.dropped = false;
+            return rv;
         }
     });
-    
-    var BucketList = Backbone.Collection.extend({ model : Bucket });
-    
+
     var BucketView = Backbone.View.extend({
+        events: {
+          'change select': 'onSelectTime'   
+        },
         
         initialize : function (options) {
-            _.bindAll(this);
-            this.parent = options.parent;
+            _.bindAll(this, "onSelectTime", "onTrashPill");
+            _.extend(this, Backbone.Events);
+            this.on("trashPill", this.onTrashPill);
+            
+            this.pillViews = {}; // keep track of anything dropped
             
             var self = this;
             this.droppable = new M.DragAndDrop.Droppable(this.el, {
@@ -46,86 +130,110 @@
                 accept: ['draggable'],
                 greedy: 'true',
                 ondrop: function (element, onto, event) { 
-                    self.parent.pillDropped(element, onto, event); 
+                    if (jQuery(self.el).children(".pill-bucket").hasClass("disabled")) {
+                        return false;
+                    } else if (jQuery(element).hasClass("trashable")) {
+                        newnode = element;
+                    } else {
+                        var newnode = element.cloneNode(true);
+                        newnode.id = "pill_" + (Math.random() + "").substring(2, 6);
+                        document.body.appendChild(newnode);
+                        
+                        jQuery(newnode).css({'float': 'none !important', 'margin': 'none', 'position': 'absolute', 'left': event._mouse.page.x - 18, 'top': event._mouse.page.y - 5, 'opacity': '1'});
+                        jQuery(newnode).addClass("trashable");
+                        
+                        // @todo initialize with the model?
+                        self.pillViews[newnode.id] = new TrashablePillView({el: newnode, parent: self});
+                    }
+                    global.dropped = true;
                 }
             });
-        }
+        },
         
+        onTrashPill : function (pillId) {
+            this.pillViews[pillId].remove();
+            delete this.pillViews[pillId];
+        },
+        
+        onSelectTime : function (evt) {
+            var srcElement = evt.srcElement || evt.target || evt.originalTarget;
+            var na = jQuery(srcElement).children("option:selected.na");
+            if (na.length) {
+                jQuery(srcElement).parent().prev(".pill-bucket").addClass("disabled");
+                for (var id in this.pillViews) {
+                    this.onTrashPill(id);
+                }
+            } else {
+                jQuery(srcElement).parent().prev(".pill-bucket").removeClass("disabled");
+            }
+        }
     });
 
     var PillGameView = Backbone.View.extend({
-        events: {},
-        
-        dropped: false,
+        events: {
+            'click .add-a-pill': 'onNewPill',
+            'click .pill-text span': 'onEdit',
+            'blur .pill-text input': 'onReadOnly'
+        },
 
         initialize: function (options) {
-            _.bindAll(this, 'render', 'pillDropped', 'revertEffect');
-            var self = this;
+            _.bindAll(this, 'addPill', 'onEdit', 'onReadOnly', 'onNewPill');
             
-            this.pillViews = [];
-            this.bucketViews = [];
+            this.pills = options.pills;
+            this.pills.bind('add', this.addPill);
             
-            // pickup the pills from the DOM or the gameState
-            if (options.mode == "practice") {
-                jQuery("div.pill").each(function () {
-                    var pill = new Pill({
-                        name: jQuery(this).children("pill-text").html()
-                    });
-                    
-                    self.pillViews.push(new PillView({ 
-                        model: pill, 
-                        el: jQuery(this).find(".draggable")[0], 
-                        ordinal: self.pillViews.length,
-                        parent: self
-                    }));
-                });
-            } else {
-            }
-            
-            // pick up all buckets from the DOM
-            jQuery("div.pill-bucket").each(function () {
-                var bucket = new Bucket({});
-                
-                self.bucketViews.push(new BucketView({ 
-                    model: bucket, 
-                    el: this,
-                    parent: self
-                }));
+            // pick up buckets from the DOM
+            jQuery("div.pill-bucket-container").each(function () {
+                new BucketView({ el: this });
             });
         },
         
-        pillDropped: function (element, onto, event) {
-            var newnode;
-            if (jQuery(element).hasClass("trashable")) {
-                newnode = element;
-            } else {
-                newnode = element.cloneNode(true);
-                newnode.id = "pill_" + (Math.random() + "").substring(2, 6);
-                document.body.appendChild(newnode);
-                jQuery(newnode).css({'float': 'none !important', 'margin': 'none', 'position': 'absolute', 'left': event._mouse.page.x - 18, 'top': event._mouse.page.y - 5, 'opacity': '1'});
-                jQuery(newnode).addClass("trashable");
-                new PillView({el: newnode, parent: this});
-            }
+        addPill: function (pill) {
+            var elt = jQuery(".pill-template").clone();
             
-            this.dropped = true;
+            jQuery(elt).attr("id", "pill_" + (Math.random() + "").substring(2, 6));
+            jQuery(elt).removeClass('pill-template'); // visible
+            jQuery(elt).addClass(pill.get("mode")); // practice || real
+            
+            var css = { 
+               'background-image': '-webkit-gradient(radial, 65% 35%, 1, center center, 30, from(#ffffff), to(' + pill.get("color") + '))'
+            };
+            
+            jQuery(elt).find(".pill-image").css(css);
+            jQuery(elt).find(".draggable").css(css);
+            jQuery(elt).find(".pill-text span").html(pill.get("text"));
+            jQuery(elt).appendTo("#pill-list");
+            
+            new DraggablePillView({ model: pill, el: jQuery(elt).find('span.draggable') });  
         },
         
-        revertEffect: function (innerelement, top_offset, left_offset) {
-            var rv;
-            if (jQuery(innerelement).hasClass("trashable")) {
-                if (!this.dropped) {
-                    document.body.removeChild(innerelement);
-                }
-            } else {
-                var dur = 0;
-                if (!this.dropped) {
-                   dur = Math.sqrt(Math.abs(top_offset ^ 2) + Math.abs(left_offset ^ 2)) * 0.02;
-                }
-                 
-                rv = new (MochiKit.Visual.Move)(innerelement, {x: - left_offset, y: - top_offset, duration: dur});
-            }
-            this.dropped = false;
-            return rv;
+        onNewPill: function (evt) {
+            this.pills.add(new Pill({ 'ordinal': this.pills.length + 1 }));
+        },
+        
+        onRemovePill: function (evt) {
+            var srcElement = evt.srcElement || evt.target || evt.originalTarget;
+            
+            // Remove this pill from view 
+            
+            // Tell the buckets to remove any views they have on this pill
+        },
+        
+        onEdit: function (evt) {
+            var srcElement = evt.srcElement || evt.target || evt.originalTarget;
+            jQuery(srcElement).next('input').show().focus();
+            jQuery(srcElement).hide();
+        },
+        
+        onReadOnly: function (evt) {
+            var srcElement = evt.srcElement || evt.target || evt.originalTarget;
+            var val = jQuery(srcElement).val();
+            console.log(val);
+            
+            if (val.length) {
+                jQuery(srcElement).prev('span').html(val).show();
+                jQuery(srcElement).hide();
+            }   
         },
 
         
@@ -159,18 +267,19 @@
     };
 
     jQuery(document).ready(function () {
-        // pick up all pills from the game state or the DOM
         var mode = jQuery("#mode").html();
         
+        var pills = new PillList();
         var pillGameView = new PillGameView({
             mode: mode,
-            el: 'div#contentcontainer'
+            el: 'div#contentcontainer',
+            pills: pills
         });
         
-        // Populate collection from the DOM & the game_state
-        // As items are added to the collection, the ListView
-        // will be signaled to create a new subview
-        var game_state = global.Intervention.getGameVar('pillgame', {});
-
+        if (mode === 'practice') {
+            pills.practiceList();
+        } else {
+            var game_state = global.Intervention.getGameVar('pillgame', {});
+        }
     });
 }(jQuery));
