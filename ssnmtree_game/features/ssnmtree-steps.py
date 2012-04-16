5429daa5a5ca458db4d083c0faa7350bd5cefb7e
sdreher <sdreher@gmail.com>
2012-04-16 10:08:31 -0400

    * full tree testing

--- ssnmtree_game/features/ssnmtree-steps.py
+++ ssnmtree_game/features/ssnmtree-steps.py
@@ -15,9 +15,93 @@ def filled_in_ssnmtree(self, text):
     if not world.using_selenium:
         assert False, "this step needs to be implemented for the django test client"
     all_filled = True
-    for i in world.firefox.find_elements_by_tag_name('input[type=text]'):
+    for i in world.firefox.find_elements_by_tag_name('input'):
         if i.get_attribute('type') == 'text':
             if i.get_attribute('value') != text:
                 all_filled = False
     assert all_filled
+    
+@step(u'When I clear the circle')
+def when_i_clear_the_circle(step):
+    fruit = world.firefox.find_element_by_id("top1-fruit")
+    elt = fruit.find_element_by_tag_name('input')
+    elt.clear()
+    
+@step(u'When I name the circle "([^"]*)"')
+def when_i_name_the_circle_name(step, name):    
+    fruit = world.firefox.find_element_by_id("top1-fruit")
+    elt = fruit.find_element_by_tag_name('input')
+    elt.clear()
+    elt.send_keys(name)
+    
+@step(u'When I click the circle')
+def when_i_click_the_circle(step):
+    fruit = world.firefox.find_element_by_id("top1-fruit")
+    elt = world.firefox.find_element_by_css_selector("div.circle")
+    assert elt != None
+    elt.click();
+    time.sleep(2)
+    
+@step(u'Then the circle is "([^"]*)"')
+def then_the_circle_is_color(step, color):
+    elt = world.firefox.find_element_by_id("top1-fruit")
+    assert elt != None
+    
+    if color == "gold":
+        gold = elt.find_element_by_css_selector("div.ripe.turned-on")
+        assert gold, "The circle is not gold"
+    elif color == "purple":
+        purple = elt.find_element_by_css_selector("div.circle.turned-on")
+        assert purple, "The circle is not purple"
+    elif color == "gold and purple":
+        gold = elt.find_element_by_css_selector("div.ripe.turned-on")
+        purple = elt.find_element_by_css_selector("div.circle.turned-on")
+        assert gold, "The circle is not gold"
+        assert purple, "The circle is not purple"
+    
+@step(u'Then the circle is not "([^"]*)"')
+def then_the_circle_is_not_color(step, color):
+    elt = world.firefox.find_element_by_id("top1-fruit")
+    assert elt != None
+    
+    try:
+        if color == "gold":
+            gold = elt.find_element_by_css_selector("div.ripe.turned-on")
+        elif color == "purple":
+            purple = elt.find_element_by_css_selector("div.circle.turned-on")
+        elif color == "gold and purple":
+            gold = elt.find_element_by_css_selector("div.ripe.turned-on")
+            purple = elt.find_element_by_css_selector("div.circle.turned-on")
+        assert False, "The circle is %s" % color
+    except:
+        pass # expected
+    
+@step(u'When I click the "([^"]*)" button')
+def when_i_click_the_name_button(step, name):
+    elt = None
+    if name == "disclosure":
+        elt = world.firefox.find_element_by_id("toggle-disclosure-selection")
+    elif name == "support": 
+        elt = world.firefox.find_element_by_id("toggle-support-selection")
+        
+    assert elt, "No button named %s found" % name
+    elt.click()
+    
+@step(u'Then "([^"]*)" is selected')
+def then_button_is_selected(step, button):
+    elts = world.firefox.find_elements_by_class_name("on")
+    assert len(elts) == 1, "There should only be one button selected. There are %s" % len(elts)
+    
+    if button == "disclosure":
+        assert elts[0].get_attribute("id") == "toggle-disclosure-selection"
+    elif button == "support": 
+        assert elts[0].get_attribute("id") == "toggle-support-selection"
+    
+@step(u'Then I wait (\d+) second')
+def then_i_wait_count_second(step, count):
+    n = int(count)
+    time.sleep(n)
+    
+        
+    
     
--- ssnmtree_game/features/ssnmtree_basics.feature
+++ ssnmtree_game/features/ssnmtree_basics.feature
@@ -17,29 +17,101 @@ Feature: SSNMTree Basics
         When I click the "Next →" link
         Then there is a game
         When I fill in the SSNM Tree with "regular"
+        There is a filled in SSNM Tree with "regular"
+        
+        # Verify state saved
         When I click the "Next →" link
+        Then I wait 1 second
         When I click the "← Back" link
         There is a filled in SSNM Tree with "regular"
         
+    Scenario: Test disclosure & support buttons
+        Using selenium
+        # Disclosure
+        Then "disclosure" is selected
+        When I click the circle
+        Then the circle is "gold"
+        When I click the circle
+        Then the circle is not "gold"
+        
+        # Support
+        When I click the "support" button
+        When I click the circle
+        Then the circle is "purple"
+        When I click the circle
+        Then the circle is not "purple"
+        
+        # Disclosure & support
+        When I click the "disclosure" button
+        When I click the circle
+        Then the circle is "gold"
+        When I click the "support" button
+        When I click the circle
+        Then the circle is "gold and purple"
+        
+        # Verify state saved
+        When I click the "Next →" link
+        Then I wait 1 second
+        When I click the "← Back" link
+        Then the circle is "gold and purple"
+        
+        # Check the clear functionality
+        Then "disclosure" is selected
+        When I clear the circle
+        Then the circle is not "gold"
+        Then the circle is not "purple"
+        
+        # Circles cannot have attributes w/o a name
+        When I click the circle
+        Then the circle is not "gold"
+        
     Scenario: Test Defaulter saving
         Using selenium
+        
+        # Need a state save event before navigate
+        # otherwise, the json data is not saved
+        When I name the circle "regular"
+        When I click the circle
+        Then the circle is "gold"
+        When I click the "support" button
+        When I click the circle
+        Then the circle is "gold and purple"
+        
         When I click the "Sessions" link
         Then I am on the Intervention page
         When I click on Session 4
         When I click on Activity 13
         When I click the "Next →" link
         Then there is a game
+        There is a filled in SSNM Tree with "regular"
+        Then the circle is "gold and purple"
         When I fill in the SSNM Tree with "defaulter"
+        Then "disclosure" is selected
+        
+        # Clearing out the name (via fill-in), clears out the attributes
+        Then the circle is not "gold"
+        Then the circle is not "purple"
+        When I click the circle
+        Then the circle is "gold"
+        
         When I click the "Next →" link
+        Then I wait 1 second
         When I click the "← Back" link
         There is a filled in SSNM Tree with "defaulter"
+        Then the circle is "gold"
+        
+        # Make sure the regular session data remains the same
         When I click the "Sessions" link
         Then I am on the Intervention page
         When I click on Session 1
         Then I click on Activity 12
         When I click the "Next →" link
         Then there is a game
-        There is a filled in SSNM Tree with "regular"
+        There is a filled in SSNM Tree with "regular"  
+        Then the circle is "purple"      
+                
+        
+        
             
          
     
