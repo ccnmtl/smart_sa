328f95fb5f4c3ed798090b8fa1fc585f864160da
Anders Pearson <anders@columbia.edu>
2012-04-18 14:19:14 -0400

    failing test for PMT #79863

--- intervention/features/intervention-steps.py
+++ intervention/features/intervention-steps.py
@@ -17,4 +17,21 @@ def there_is_not_an_edit_deployment_form(step):
             found = True
     assert not found
 
+@step(u'there is a counselors table')
+def there_is_a_counselors_table(step):
+    try:
+        t = world.dom.get_element_by_id("counselors")
+        assert True
+    except KeyError:
+        assert False, 'no counselors table visible to admin'
+
+@step(u'there is not a counselors table')
+def there_is_not_a_counselors_table(step):
+    try:
+        t = world.dom.get_element_by_id("counselors")
+        assert False, "counselors table is visible to non-admin"
+    except KeyError:
+        assert True
+
+
 
