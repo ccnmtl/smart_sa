620644d9646790792183e4a5ca5a69d74955a108
Anders Pearson <anders@columbia.edu>
2012-04-26 16:17:35 -0400

    try to drop a db first (at least for now)

--- initialize.bat
+++ initialize.bat
@@ -1,5 +1,6 @@
 set HOME=C:\smart_sa\
 cd C:\smart_sa\
+"C:\Program Files\Bitnami DjangoStack\postgresql\bin\dropdb.exe" -U postgres masivukeni
 "C:\Program Files\Bitnami DjangoStack\postgresql\bin\createdb.exe" -U postgres masivukeni
 "C:\Program Files\Bitnami DjangoStack\Python\python.exe" manage.py syncdb --settings=settings_windows
 "C:\Program Files\Bitnami DjangoStack\Python\python.exe" manage.py migrate intervention --settings=settings_windows
