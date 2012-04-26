fdd291c27d21b23285b40e4bb9fd09c87d77ec5a
Anders Pearson <anders@columbia.edu>
2012-04-26 16:18:49 -0400

    do not prompt to create a superuser

--- initialize.bat
+++ initialize.bat
@@ -2,7 +2,7 @@ set HOME=C:\smart_sa\
 cd C:\smart_sa\
 "C:\Program Files\Bitnami DjangoStack\postgresql\bin\dropdb.exe" -U postgres masivukeni
 "C:\Program Files\Bitnami DjangoStack\postgresql\bin\createdb.exe" -U postgres masivukeni
-"C:\Program Files\Bitnami DjangoStack\Python\python.exe" manage.py syncdb --settings=settings_windows
+"C:\Program Files\Bitnami DjangoStack\Python\python.exe" manage.py syncdb --noinput --settings=settings_windows
 "C:\Program Files\Bitnami DjangoStack\Python\python.exe" manage.py migrate intervention --settings=settings_windows
 "C:\Program Files\Bitnami DjangoStack\Python\python.exe" manage.py migrate --settings=settings_windows
 "C:\Program Files\Bitnami DjangoStack\Python\python.exe" manage.py migrate --settings=settings_windows
