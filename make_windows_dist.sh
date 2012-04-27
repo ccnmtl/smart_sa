da7f974c79fd00f225babfa958defa3db4183117
anders <anders@columbia.edu>
2012-04-26 21:22:18 -0400

    pull down intervention data to include in windows deployment

--- make_windows_dist.sh
+++ make_windows_dist.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+./manage.py pull_down
 rm -rf /mnt/key/smart_sa
 rsync -a -z -C --delete --verbose --exclude '*~' --exclude ve --exclude .git --exclude '*.pyc' ../smart_sa /mnt/key/
 
