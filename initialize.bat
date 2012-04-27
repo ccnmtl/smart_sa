7ca2ed949b159404be02375d285cf1a0a75f2cd5
anders <anders@columbia.edu>
2012-04-26 21:44:29 -0400

    fixture for clinic admin account

--- initialize.bat
+++ initialize.bat
@@ -7,4 +7,4 @@ cd C:\smart_sa\
 "C:\Program Files\Bitnami DjangoStack\Python\python.exe" manage.py migrate --settings=settings_windows
 "C:\Program Files\Bitnami DjangoStack\Python\python.exe" manage.py migrate --settings=settings_windows
 "C:\Program Files\Bitnami DjangoStack\Python\python.exe" manage.py load_intervention_content --settings=settings_windows
-
+"C:\Program Files\Bitnami DjangoStack\Python\python.exe" manage.py loaddata --settings=settings_windows intervention/fixtures/admins.json
