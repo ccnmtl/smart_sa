73fd026f7019e4ce8cd49bef79b01e7f3020b5b3
anders <anders@columbia.edu>
2012-04-26 21:29:12 -0400

    then load the data in on the windows side

---
+++ intervention/management/commands/load_intervention_content.py
@@ -0,0 +1,53 @@
+from django.core.management.base import BaseCommand
+from intervention.models import Intervention
+from problemsolving_game.models import Issue
+from django.conf import settings
+from restclient import GET
+from zipfile import ZipFile
+from cStringIO import StringIO
+from simplejson import dumps,loads
+import os
+import os.path
+from optparse import make_option
+
+class Command(BaseCommand):
+    args = ''
+    help = ''
+
+    option_list = BaseCommand.option_list + (
+        make_option('-d', '--db-only', dest='dbonly',
+                    default=False,help='only pull the database content'),
+    )
+
+    def handle(self, *args, **options):
+        if not settings.DEBUG:
+            print "this should never be run on production"
+            return
+
+        zc = open(os.path.join("data","intervention.zip")).read()
+        buffer = StringIO(zc)
+        zipfile = ZipFile(buffer,"r")
+        
+        # Load Intervention objects
+        json = loads(zipfile.read("interventions.json"))
+
+        print "clearing intervention prod database content..."
+        Intervention.objects.all().delete()
+
+        print "importing prod database content..."
+        for i in json['interventions']:
+            intervention = Intervention.objects.create(name="tmp")
+            intervention.from_dict(i)
+            
+        # Load Problem Solving objects
+        json = loads(zipfile.read("issues.json"))
+
+        print "clearing problemsolving database content..."
+        Issue.objects.all().delete()
+
+        print "importing problemsolving prod database content..."
+        for i in json['issues']:
+            issue = Issue.objects.create(name="tmp",ordinality=0)
+            issue.from_dict(i)
+    
+            
