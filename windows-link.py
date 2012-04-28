4896680eaf5120771a6935c7f2cb10fa7762310a
Anders Pearson <anders@columbia.edu>
2012-04-28 11:41:15 -0400

    clearing out old files that are no longer used

--- windows-link.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import sys
-
-if __name__ == '__main__':
-    print """cd "C:\\Program Files\\Mozilla Firefox"\r
-"C:\\Program Files\\Mozilla Firefox\\firefox.exe" "file://localhost/C:/Documents and Settings/Administrator/My Documents/%s/%s/home.html"\r
-""" % (sys.argv[1],sys.argv[1])
-
