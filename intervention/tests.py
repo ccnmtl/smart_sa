4652a219a2b5004864310c45779eb1f2989637e9
Anders Pearson <anders@columbia.edu>
2012-05-07 07:36:45 -0400

    starting some unit tests

---
+++ intervention/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+class IndexViewTest(TestCase):
+    def test_index(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+class InterventionViewTest(TestCase):
+    def test_index(self):
+        resp = self.client.get('/intervention/')
+        self.assertEqual(resp.status_code, 302)
