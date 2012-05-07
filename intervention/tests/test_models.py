a8138b8184b13d22110ccb226cb239633b6b2015
Anders Pearson <anders@columbia.edu>
2012-05-07 08:35:40 -0400

    clientsession unit tests

--- intervention/tests/test_models.py
+++ intervention/tests/test_models.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from smart_sa.intervention.models import ClientSession
 from smart_sa.intervention.models import Deployment
 from smart_sa.intervention.models import Intervention
 
@@ -13,20 +14,53 @@ class InterventionModelTest(TestCase):
 
     def test_isolated_serialization(self):
         d = self.i.as_dict()
+        # check the dict
         self.assertEqual(d['name'],"test intervention")
         self.assertEqual(d['intervention_id'],"1")
         self.assertEqual(d['general_instructions'],"this is for testing")
         i2 = Intervention.objects.create(name="test2",
                                          intervention_id="2",
                                          general_instructions="number 2")
-
+        # check round-trip
         i2.from_dict(d)
         self.assertEqual(unicode(i2),"test intervention")
         self.assertEqual(i2.intervention_id,"1")
         self.assertEqual(i2.general_instructions,"this is for testing")
 
 class ClientSessionModelTest(TestCase):
-    pass
+    def setUp(self):
+        self.i = Intervention.objects.create(name="test intervention",
+                                             intervention_id="1",
+                                             general_instructions="this is for testing")
+        self.cs = ClientSession.objects.create(intervention = self.i,
+                                               short_title = "Test Session 1",
+                                               long_title = "Test Session 1 Long Title",
+                                               introductory_copy = "Introductory Copy Here",
+                                               defaulter = False)
+
+    def test_basics(self):
+        self.assertEqual(self.cs.intervention,self.i)
+        self.assertEqual(unicode(self.cs),"Test Session 1")
+        self.assertEqual(self.cs.get_absolute_url().startswith("/session/"),True)
+        self.assertEqual(self.cs.index(),1)
+
+    def test_isolated_serialization(self):
+        d = self.cs.as_dict()
+        self.assertEqual(d['short_title'],"Test Session 1")
+        self.assertEqual(d['long_title'],"Test Session 1 Long Title")
+        self.assertEqual(d['introductory_copy'],"Introductory Copy Here")
+        self.assertEqual(d['defaulter'],False)
+        # try round-tripping
+        cs2 = ClientSession.objects.create(intervention = self.i,
+                                           short_title = "Test Session 2",
+                                           long_title = "Test Session 2 Long Title",
+                                           introductory_copy = "Introductory Copy 2 Here",
+                                           defaulter = True)
+        cs2.from_dict(d)
+        self.assertEqual(unicode(cs2),"Test Session 1")
+        self.assertEqual(cs2.long_title,"Test Session 1 Long Title")
+        self.assertEqual(cs2.introductory_copy,"Introductory Copy Here")
+        self.assertEqual(cs2.defaulter,False)
 
 class ActivityModelTest(TestCase):
     pass
