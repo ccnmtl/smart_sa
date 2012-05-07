b4ab0c4d863ca7f6ea17d0984ff9ae511980627e
Anders Pearson <anders@columbia.edu>
2012-05-07 08:19:38 -0400

    adding unit tests

--- intervention/tests/test_models.py
+++ intervention/tests/test_models.py
@@ -1,9 +1,41 @@
 from django.test import TestCase
 from smart_sa.intervention.models import Deployment
+from smart_sa.intervention.models import Intervention
 
 class InterventionModelTest(TestCase):
-    def test_as_dict(self):
-        self.assertEqual(200, 200)
+    def setUp(self):
+        self.i = Intervention.objects.create(name="test intervention",
+                                             intervention_id="1",
+                                             general_instructions="this is for testing")
+    def test_basics(self):
+        self.assertEqual(unicode(self.i),"test intervention")
+        self.assertEqual(self.i.get_absolute_url().startswith("/intervention/"),True)
+
+    def test_isolated_serialization(self):
+        d = self.i.as_dict()
+        self.assertEqual(d['name'],"test intervention")
+        self.assertEqual(d['intervention_id'],"1")
+        self.assertEqual(d['general_instructions'],"this is for testing")
+        i2 = Intervention.objects.create(name="test2",
+                                         intervention_id="2",
+                                         general_instructions="number 2")
+
+        i2.from_dict(d)
+        self.assertEqual(unicode(i2),"test intervention")
+        self.assertEqual(i2.intervention_id,"1")
+        self.assertEqual(i2.general_instructions,"this is for testing")
+
+class ClientSessionModelTest(TestCase):
+    pass
+
+class ActivityModelTest(TestCase):
+    pass
+
+class InstructionModelTest(TestCase):
+    pass
+
+class GamePageModelTest(TestCase):
+    pass
 
 class DeploymentModelTest(TestCase):
     def test_online(self):
@@ -13,3 +45,21 @@ class DeploymentModelTest(TestCase):
         self.assertEqual(d1.is_clinic(),False)
         self.assertEqual(d2.is_online(),False)
         self.assertEqual(d2.is_clinic(),True)
+
+class ParticipantModelTest(TestCase):
+    pass
+
+class ParticipantSessionModelTest(TestCase):
+    pass
+
+class ParticipantActivityModelTest(TestCase):
+    pass
+
+class CounselorNoteModelTest(TestCase):
+    pass
+
+class ParticipantGameVarModelTest(TestCase):
+    pass
+
+class BackupModelTest(TestCase):
+    pass
