5f1688f4a55d92530ba49d7858020cff0686b1dc
Anders Pearson <anders@columbia.edu>
2012-05-09 04:15:34 -0400

    test clear participant

--- intervention/tests/test_views.py
+++ intervention/tests/test_views.py
@@ -48,3 +48,87 @@ class InterventionViewTest(TestCase):
         for a in Activity.objects.all():
             resp = self.client.get(a.get_absolute_url())
             self.assertEqual(resp.status_code, 200)
+
+    def test_clear_participant(self):
+        # make sure we have one logged in
+        resp = self.client.post('/set_participant/', {'name': 'test', 'id_number': 'test'})
+        # now clear it
+        resp = self.client.get('/clear_participant/', follow=True)
+        # try hitting a session page and make sure we get redirected
+        s = ClientSession.objects.all()[0]
+        resp = self.client.get(s.get_absolute_url(), follow=True)
+        (url,status) = resp.redirect_chain[0]
+        self.assertEqual(status,302)
+        self.assertEqual("/set_participant/" in url, True)
+
+    # def test_login_nonexistant_participant(self):
+    #     pass
+
+    # def test_login_inactive_participant(self):
+    #     pass
+
+    # def test_login_participant_with_wrong_password(self):
+    #     pass
+
+    # def test_set_deployment(self):
+    #     pass
+
+    # def test_practice_mode(self):
+    #     pass
+
+    # def test_manage_participants(self):
+    #     pass
+
+    # def test_add_participant(self):
+    #     pass
+
+    # def test_delete_participant(self):
+    #     pass
+
+    # def test_edit_participant(self):
+    #     pass
+
+    # def test_edit_counselor(self):
+    #     pass
+
+    # def test_view_participant(self):
+    #     pass
+
+    # def test_add_counselor(self):
+    #     pass
+
+    # def test_participant_data_download(self):
+    #     pass
+
+    # def test_download_backup(self):
+    #     pass
+
+    # def test_restore_participants(self):
+    #     pass
+
+    # def test_upload_participant_data(self):
+    #     pass
+
+    # def test_update_intervention_content(self):
+    #     pass
+
+    # def test_complete_session(self):
+    #     pass
+
+    # def test_complete_activity(self):
+    #     pass
+
+    # def test_save_game_state(self):
+    #     pass
+
+    # def test_intervention_admin(self):
+    #     pass
+
+    # def test_activity_admin(self):
+    #     pass
+
+    # def test_gamepage_admin(self):
+    #     pass
+
+    # def test_zip_download(self):
+    #     pass
