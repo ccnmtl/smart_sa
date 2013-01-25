77787f5f773a2ad1d7a63c7a22e33b3bb5162e02
Anders Pearson <anders@columbia.edu>
2013-01-25 11:52:32 -0500

    sentry -> raven

--- settings_production.py
+++ settings_production.py
@@ -23,16 +23,20 @@ DATABASES = {
 INTERVENTION_BACKUP_HEXKEY = "d37fb81dff7672c76f4881a8f57c002403ba2ce5155dc4ac6b68a2d9caa51d88"
 INTERVENTION_BACKUP_IV = "899f6762313185a9593480e6f015b0d5053464daa5ecadd00dc4e7e2984f028f"
 
+SENTRY_SITE = 'masivukeni'
+SENTRY_SERVERS = ['http://sentry.ccnmtl.columbia.edu/sentry/store/']
+
 import logging
-from sentry.client.handlers import SentryHandler
+from raven.contrib.django.handlers import SentryHandler
 logger = logging.getLogger()
-if SentryHandler not in map(lambda x: x.__class__, logger.handlers):
+# ensure we havent already registered the handler
+if SentryHandler not in map(type, logger.handlers):
     logger.addHandler(SentryHandler())
+
+    # Add StreamHandler to sentry's default so you can catch missed exceptions
     logger = logging.getLogger('sentry.errors')
     logger.propagate = False
     logger.addHandler(logging.StreamHandler())
-SENTRY_REMOTE_URL = 'http://sentry.ccnmtl.columbia.edu/sentry/store/'
-SENTRY_SITE = 'masivukeni2' # can't rely on the sites framework when a transaction is aborted
 
 try:
     from local_settings import *
