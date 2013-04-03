34f4b70b664d2cef553044035459fe344f8e6284
Anders Pearson <anders@columbia.edu>
2013-04-03 15:51:50 -0400

    production config tweaks for moving to patty

--- apache/django.wsgi
+++ apache/django.wsgi
@@ -1,10 +1,7 @@
 import os, sys, site
 
-# enable the virtualenv
-site.addsitedir('/var/www/masivukeni2/smart_sa/ve/lib/python2.6/site-packages')
-
 # paths we might need to pick up the project's settings
-sys.path.append('/var/www/masivukeni2/smart_sa/')
+sys.path.append('/var/www/masivukeni/smart_sa/')
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'smart_sa.settings_production'
 
--- smart_sa/settings_production.py
+++ smart_sa/settings_production.py
@@ -1,10 +1,10 @@
 # flake8: noqa
 from settings_shared import *
 TEMPLATE_DIRS = (
-    "/var/www/masivukeni2/smart_sa/smart_sa/templates",
+    "/var/www/masivukeni/smart_sa/smart_sa/templates",
 )
 
-MEDIA_ROOT = '/var/www/masivukeni2/uploads/'
+MEDIA_ROOT = '/var/www/masivukeni/uploads/'
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
@@ -27,17 +27,16 @@ INTERVENTION_BACKUP_IV = "899f6762313185a9593480e6f015b0d5053464daa5ecadd00dc4e7
 SENTRY_SITE = 'masivukeni'
 SENTRY_SERVERS = ['http://sentry.ccnmtl.columbia.edu/sentry/store/']
 
-import logging
-from raven.contrib.django.handlers import SentryHandler
-logger = logging.getLogger()
-# ensure we havent already registered the handler
-if SentryHandler not in map(type, logger.handlers):
-    logger.addHandler(SentryHandler())
-
-    # Add StreamHandler to sentry's default so you can catch missed exceptions
-    logger = logging.getLogger('sentry.errors')
-    logger.propagate = False
-    logger.addHandler(logging.StreamHandler())
+if 'migrate' not in sys.argv:
+    import logging
+    from raven.contrib.django.handlers import SentryHandler
+    logger = logging.getLogger()
+    # ensure we havent already registered the handler
+    if SentryHandler not in map(type, logger.handlers):
+        logger.addHandler(SentryHandler())
+        logger = logging.getLogger('sentry.errors')
+        logger.propagate = False
+        logger.addHandler(logging.StreamHandler())
 
 try:
     from local_settings import *
