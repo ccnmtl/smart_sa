a9ec5527eb753d31d75c16af914de8b8b413ad29
Anders Pearson <anders@columbia.edu>
2016-03-14 10:19:56 +0000

    :arrow_up: ccnmtlsettings 0.1.6
    passwords in fixtures need to be overwritten since 0.1.6 changes the `PASSWORD_HASHERS`

--- smart_sa/dashboard/tests/test_views.py
+++ smart_sa/dashboard/tests/test_views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 
@@ -5,6 +6,9 @@ class IndexViewTest(TestCase):
     fixtures = ["full_testdb.json"]
 
     def setUp(self):
+        u = User.objects.get(username='testcounselor')
+        u.set_password('test')
+        u.save()
         self.client.login(username='testcounselor', password='test')
 
     def test_index(self):
