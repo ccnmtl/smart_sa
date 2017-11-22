11431b0af19a58f49a18291eeb7e753b913e19ac
Nick Buonincontri <nbuonin@gmail.com>
2017-11-22 16:43:17 -0500

    Remove uneeded dashboard app

--- smart_sa/dashboard/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from .views import index, download, participant
-
-urlpatterns = [
-    url(r'^$', index),
-    url(r'^download/$', download),
-    url(r'^participant/(?P<participant_id>\d+)/$', participant),
-]
