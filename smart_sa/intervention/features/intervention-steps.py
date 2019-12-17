f79724a9bc2934ca7031d3547b67edb4c43e4640
Susan Dreher <sdreher@gmail.com>
2019-12-17 11:21:55 -0500

    features directory needs a __init__.py if using supporting steps code

--- smart_sa/intervention/features/intervention-steps.py
+++
@@ -1,9 +0,0 @@
-# flake8: noqa
-
-from lettuce import world, step
-from smart_sa.intervention.models import Participant, Deployment
-from lettuce.django import django_url
-try:
-    from lxml import html  # nosec
-except ImportError:
-    pass
