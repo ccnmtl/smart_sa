5defe17ae2020b13be4dbe3fb4c769f0a2694380
Susan Dreher <sdreher@gmail.com>
2022-01-20 18:55:54 -0500

    Remove old virtualenv files

--- admin_initialize.bat
+++
@@ -1,3 +0,0 @@
-cd C:\smart_sa\
-"C:\Program Files\Bitnami DjangoStack\Python\python.exe" pip.py install -r requirements\windows.txt
-copy apache\bitnami.conf "C:\Program Files\Bitnami DjangoStack\apps\django\conf\django.conf"
